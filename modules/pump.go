d584a38c40eb228c49e0f2eb6f65f4e341538b83
Ranjib Dey <ranjib@linux.com>
2016-06-15 22:32:38 -0700

    add logging statement when pump state is changed

--- modules/pump.go
+++ modules/pump.go
@@ -1,6 +1,7 @@
 package modules
 
 import (
+	log "github.com/Sirupsen/logrus"
 	"github.com/hybridgroup/gobot/platforms/gpio"
 	"github.com/hybridgroup/gobot/platforms/raspi"
 )
@@ -13,11 +14,13 @@ type Pump struct {
 func (p *Pump) On() error {
 	r := raspi.NewRaspiAdaptor("raspi")
 	pin := gpio.NewDirectPinDriver(r, "pump", p.Pin)
+	log.Infoln("Turning on pump via pin:", p.Pin)
 	return pin.DigitalWrite(byte(1))
 }
 
 func (p *Pump) Off() error {
 	r := raspi.NewRaspiAdaptor("raspi")
 	pin := gpio.NewDirectPinDriver(r, "pump", p.Pin)
+	log.Infoln("Turning off pump via pin:", p.Pin)
 	return pin.DigitalWrite(byte(0))
 }
