bf8581c413022e5c13b3d60aca145268376bfcb6
Ranjib Dey <dey.ranjib@gmail.com>
2016-07-31 00:47:12 -0700

    Device mockup (#20)
    * use example config for development
    
    * Make auth optional
    
    * bring react. add device specific sections in home page

---
+++ assets/js/reefer.js
@@ -0,0 +1,10 @@
+var Device = React.createClass({
+  render: function(){
+    return (
+    );
+  }
+});
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('screenshot')
+);
--- webui/server.go
+++ webui/server.go
@@ -16,16 +16,26 @@ type Server struct {
 	ImageDirectory   string
 }
 
-func (s *Server) Setup() {
+func (s *Server) Setup(auth bool) {
 	authProvider := google.New(s.OauthID, s.OauthSecret, s.OauthCallbackUrl)
 	gomniauth.SetSecurityKey(s.GomniAuthSecret)
 	gomniauth.WithProviders(authProvider)
 	assets := http.FileServer(http.Dir("assets"))
 	images := http.FileServer(http.Dir(s.ImageDirectory))
-	http.Handle("/login", &templateHandler{filename: "login.html"})
-	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
-	http.Handle("/images/", http.StripPrefix("/images/", images))
-	http.HandleFunc("/auth/", s.loginHandler)
-	http.HandleFunc("/logout", logoutHandler)
-	http.Handle("/", MustAuth(&homePageHandler{}))
+
+	if auth {
+		http.Handle("/", MustAuth(&homePageHandler{}))
+		http.Handle("/assets/", MustAuth(http.StripPrefix("/assets/", assets)))
+		http.Handle("/images/", MustAuth(http.StripPrefix("/images/", images)))
+
+		// Auth specific paths
+		http.HandleFunc("/auth/", s.loginHandler)
+		http.HandleFunc("/logout", logoutHandler)
+		http.Handle("/login", &templateHandler{filename: "login.html"})
+	} else {
+		http.Handle("/", &homePageHandler{})
+		http.Handle("/assets/", http.StripPrefix("/assets/", assets))
+		http.Handle("/images/", http.StripPrefix("/images/", images))
+	}
+
 }
