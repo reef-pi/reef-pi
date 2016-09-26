647f0eaf0f7a4ac7a454c0f0914f44e05030b9a8
Ranjib Dey <dey.ranjib@gmail.com>
2016-09-25 22:14:12 -0700

    use on /off api calls directly (#32)

--- controller/relay.go
+++ controller/relay.go
@@ -20,12 +20,12 @@ func NewRelay(name string, conn gobot.Connection, pin string) *Relay {
 
 func (r *Relay) On() error {
 	log.Printf("Device[%s] On\n", r.Name())
-	return r.pin.DigitalWrite(byte(1))
+	return r.pin.On()
 }
 
 func (r *Relay) Off() error {
 	log.Printf("Device[%s] Off\n", r.Name())
-	return r.pin.DigitalWrite(byte(0))
+	return r.pin.Off()
 }
 func (r *Relay) Name() string {
 	return r.name
