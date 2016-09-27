c251a373d3e08e43e9ec985116dddc4418a3fe4a
Ranjib Dey <ranjib@linux.com>
2016-09-26 17:48:19 -0700

    add webpack and other npm goodies

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'assets/public');
+var APP_DIR = path.resolve(__dirname, 'assets/jsx');
+
+var config = {
+  entry: APP_DIR + '/reefer.jsx',
+	module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  }
+
+};
+
+module.exports = config;
