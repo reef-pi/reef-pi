946983af5709a0e439109c05f3840d5543a66e25
Ranjib Dey <ranjib@linux.com>
2016-12-09 08:05:17 -0800

    add jobs tab. refactor electronics

---
+++ assets/jsx/jobs.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Jobs extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    onChange(){
+    }
+
+    render() {
+      return (
+          <div>
+           Jobs list goes here
+          </div>
+          );
+    }
+}
