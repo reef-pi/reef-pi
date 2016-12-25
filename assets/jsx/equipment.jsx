08d695cff1af092584e5a212414b4864dae53984
Ranjib Dey <ranjib@linux.com>
2016-12-25 13:12:48 -0800

    update ux to support passing pwm values

--- assets/jsx/equipment.jsx
+++ assets/jsx/equipment.jsx
@@ -6,10 +6,14 @@ export default class Equipment extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          outlet:{}
+          outlet: {},
+          action: 'on',
+          value: ''
         };
         this.fetchEquipment = this.fetchEquipment.bind(this);
         this.fetchOutlet = this.fetchOutlet.bind(this);
+        this.setValue = this.setValue.bind(this);
+        this.configureEquipment = this.configureEquipment.bind(this);
     }
 
     fetchEquipment(){
@@ -23,6 +27,31 @@ export default class Equipment extends React.Component {
           error: function(xhr, status, err) {
             console.log(err.toString());
           }.bind(this)
+      });
+    }
+
+    configureEquipment() {
+      $.ajax({
+          url: '/api/outlets/' + this.state.outlet.id + '/configure',
+          type: 'POST',
+          data: JSON.stringify({
+            action: this.state.action,
+            value: Number(this.state.value)
+          }),
+          success: function(data) {
+            this.setState({
+              action: this.state.action == "on" ? "off": "on"
+            });
+          }.bind(this),
+          error: function(xhr, status, err) {
+            console.log(err.toString());
+          }.bind(this)
+      });
+    }
+
+    setValue(e){
+      this.setState({
+        value: parseInt(e.target.value)
       });
     }
 
@@ -46,13 +75,16 @@ export default class Equipment extends React.Component {
       this.fetchEquipment();
     }
 
-
     render() {
+      var rangeStyle = {};
+
+      if(this.state.outlet.type != "pwm"){
+        rangeStyle['display'] = 'none';
+      }
       return (
           <div>
-            Board: {this.state.outlet.board}
-            Pin: {this.state.outlet.pin}
-            Type: {this.state.outlet.type}
+            <input type="range" style={rangeStyle} onChange={this.setValue} value={this.state.value}/>
+            <input type="button" value={this.state.action} onClick={this.configureEquipment}/>
           </div>
           );
     }
