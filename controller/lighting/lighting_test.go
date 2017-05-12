b5a5227bf9459e33007c844b0d23a3fe5aed60d6
Ranjib Dey <ranjib@linux.com>
2017-05-11 22:30:06 -0700

    fix tests

---
+++ controller/lighting/lighting_test.go
@@ -0,0 +1,18 @@
+package lighting
+
+import (
+	"testing"
+	"time"
+)
+
+func TestGetCurrentValue(t *testing.T) {
+	intensities := []int{0, 0, 0, 0, 20, 40, 60, 70, 30, 10, 0, 0}
+	if err := ValidateValues(intensities); err != nil {
+		t.Fatal(err)
+	}
+	t1 := time.Date(2015, time.October, 12, 0, 0, 0, 0, time.UTC)
+	v := GetCurrentValue(t1, intensities)
+	if v != 0 {
+		t.Fatal("Expected 0, found:", v)
+	}
+}
