65bb06d602a7b5e1ea99654065b71cf5b476fc4c
Ranjib Dey <ranjib@linux.com>
2017-08-22 01:01:46 -0700

    refactor led channel ui

--- jsx/light.jsx
+++ jsx/light.jsx
@@ -1,27 +1,22 @@
 import React from 'react'
 import $ from 'jquery'
 import LEDChannel from './led_channel.jsx'
-import LightSlider from './light_slider.jsx'
 
 export default class Light extends React.Component {
   constructor (props) {
     super(props)
-    console.log('Removehook:', props.removeHook)
     this.state = {
       channels: {},
       fixed: {},
-      updated: false,
-      cycleEnable: false,
-      auto: false
+      updated: false
     }
     this.updateValues = this.updateValues.bind(this)
     this.getValues = this.getValues.bind(this)
-    this.getFixedValue = this.getFixedValue.bind(this)
-    this.updateFixedValue = this.updateFixedValue.bind(this)
     this.channelList = this.channelList.bind(this)
     this.fetchData = this.fetchData.bind(this)
     this.setLightMode = this.setLightMode.bind(this)
     this.updateLight = this.updateLight.bind(this)
+    this.updateChannel = this.updateChannel.bind(this)
   }
   updateLight () {
     $.ajax({
@@ -30,8 +25,7 @@ export default class Light extends React.Component {
       data: JSON.stringify({
         name: this.props.name,
         channels: this.state.channels,
-        jack: this.props.jack,
-        auto: this.state.auto
+        jack: this.props.jack
       }),
       success: function (data) {
         this.fetchData()
@@ -49,7 +43,6 @@ export default class Light extends React.Component {
       this.setState({
         channels: channels
       })
-      console.log('Pin:', pin, 'Channel', channels[pin])
     }.bind(this))
   }
 
@@ -64,7 +57,6 @@ export default class Light extends React.Component {
       success: function (data) {
         this.setState({
           channels: data.channels,
-          auto: data.auto,
           jack: data.jack
         })
       }.bind(this),
@@ -91,42 +83,25 @@ export default class Light extends React.Component {
     })
   }
 
-  getFixedValue (pin) {
+  updateChannel (pin) {
     return (
-      function () {
-        return this.state.channels[pin].fixed
+      function (ch) {
+        var channels = this.state.channels
+        channels[pin] = ch
+        this.setState({
+          channels: channels
+        })
       }.bind(this)
     )
-  }
-
-  updateFixedValue (pin, value) {
-    var channels = this.state.channels
-    channels[pin].fixed = value
-    this.setState({
-      channels: channels,
-      updated: true
-    })
   }
 
   channelList () {
     var channelUIs = []
     for (var pin in this.state.channels) {
       var ch = this.state.channels[pin]
-      var fixedStyle = {
-        display: ch.auto ? 'none' : 'block'
-      }
-      var cycleStyle = {
-        display: ch.auto ? 'block' : 'none'
-      }
       channelUIs.push(
         <div className='container' key={this.props.name + '-' + ch.name}>
-          Auto<input name={this.props.name + '-' + ch.name + '-check'} type='checkbox' onChange={this.setLightMode(pin)} value='auto' checked={this.state.auto} />
-          <div className='container' style={cycleStyle}>
-            <LEDChannel pin={pin} name={ch.name} onChange={this.updateValues} getValues={this.getValues(pin)} />
-          </div>
-          <div className='container' style={fixedStyle}>
-            <LightSlider pin={pin} name={ch.name} onChange={this.updateFixedValue} getValue={this.getFixedValue(pin)} style={fixedStyle} />
-          </div>
+          <LEDChannel pin={pin} name={ch.name} onChange={this.updateValues} ch={ch} updateChannel={this.updateChannel(pin)} />
         </div>
       )
     }
