3f883f25bb8acfe1dfdfd5cf48c731fe229a8d0a
Ranjib Dey <ranjib@linux.com>
2017-10-12 01:21:54 -0700

    remove superflusou build tags

--- controller/health.go
+++ controller/health.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"github.com/reef-pi/reef-pi/controller/utils"
+	"github.com/shirou/gopsutil/load"
+	"github.com/shirou/gopsutil/mem"
 	"log"
 	"time"
 )
@@ -18,6 +20,22 @@ func NewHealthChecker(i time.Duration, telemetry *utils.Telemetry) *HealthChecke
 		stopCh:    make(chan struct{}),
 		telemetry: telemetry,
 	}
+}
+func (h *HealthChecker) check() {
+	loadStat, err := load.Avg()
+	if err != nil {
+		log.Println("ERROR: Failed to obtain load average. Error:", err)
+		return
+	}
+	h.telemetry.EmitMetric("system_load5", loadStat.Load5)
+
+	vmStat, err := mem.VirtualMemory()
+	if err != nil {
+		log.Println("ERROR: Failed to obtain memory stats. Error:", err)
+		return
+	}
+	h.telemetry.EmitMetric("system_mem_free", vmStat.Free)
+	log.Println("health check: Free memory:", vmStat.Free, " Load5:", loadStat.Load5)
 }
 
 func (h *HealthChecker) Stop() {
