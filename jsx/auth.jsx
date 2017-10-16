b1a441c33ca3ea4b98004bef2bf318e053f384c6
Ranjib Dey <ranjib@linux.com>
2017-10-16 08:28:15 -0700

    basic auth stub ui

---
+++ jsx/auth.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Common from './common.jsx'
+
+export default class Auth extends Common {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div className='container' />
+    )
+  }
+}
