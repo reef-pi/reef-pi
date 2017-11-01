6230ee1cbaa69f2ebc7d645f0f2f07da0843a8d0
Ranjib Dey <dey.ranjib@gmail.com>
2017-11-01 08:13:18 -0700

    (chore)Jsx refactor (#235)
    * extract summary into its own component
    
    * one line summary
    
    * more temp bug
    
    * style fixes

---
+++ jsx/summary.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Common from './common.jsx'
+
+export default class Summary extends Common {
+  constructor (props) {
+    super(props)
+    this.state = {
+      info: {}
+    }
+    this.refresh = this.refresh.bind(this)
+  }
+
+  refresh () {
+    this.ajaxGet({
+      url: '/api/info',
+      success: function (data) {
+        this.setState({
+          info: data
+        })
+      }.bind(this)
+    })
+  }
+
+  componentWillMount () {
+    this.refresh()
+    var timer = window.setInterval(this.refresh, 1800 * 1000)
+    this.setState({timer: timer})
+  }
+
+  componentWillUnmount () {
+    window.clearInterval(this.state.timer)
+  }
+
+  render () {
+    return (
+      <div className='container'>
+        {super.render()}
+        <ul className='list-inline'>
+          <li className='list-inline-item'>{this.state.info.current_time},</li>
+          <li className='list-inline-item'>running <span className='text-primary'>{this.state.info.version}</span></li>
+          <li className='list-inline-item'>, since {this.state.info.uptime}</li>
+          <li className='list-inline-item'>IP <span className='text-primary'>{this.state.info.ip}</span></li>
+        </ul>
+      </div>
+    )
+  }
+}
