2badbb09afad7805e08dd450aafadd4d4fada2e9
Ranjib Dey <ranjib@linux.com>
2017-11-01 21:14:25 -0700

    provide equipmengt usage and temperature reading from same api

--- jsx/temperature_chart.jsx
+++ jsx/temperature_chart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ComposedChart,Line, Tooltip, YAxis, XAxis, AreaChart, Area, BarChart, Bar, ReferenceLine} from 'recharts'
+import {ComposedChart, Line, Tooltip, YAxis, XAxis, Bar, ReferenceLine} from 'recharts'
 import Common from './common.jsx'
 import $ from 'jquery'
 
@@ -7,7 +7,6 @@ export default class TemperatureChart extends Common {
   constructor (props) {
     super(props)
     this.state = {
-      readings: [],
       usage: []
     }
     this.fetch = this.fetch.bind(this)
@@ -25,24 +24,12 @@ export default class TemperatureChart extends Common {
 
   fetch () {
     this.ajaxGet({
-      url: '/api/tc/readings',
-      success: function (data) {
-        this.setState({
-          readings: data,
-          showAlert: false
-        })
-      }.bind(this)
-    })
-    this.ajaxGet({
       url: '/api/tc/usage',
       success: function (data) {
         var processed = []
         $.each(data, function (i, v) {
-          processed.push({
-            heater: v.heater,
-            cooler: -1 * v.cooler,
-            hour: v.hour
-          })
+          v.cooler *= -1
+          processed.push(v)
         })
         this.setState({
           usage: processed,
@@ -53,23 +40,22 @@ export default class TemperatureChart extends Common {
   }
 
   render () {
-    if (this.state.readings.length <= 0) {
+    if (this.state.usage.length <= 0) {
       return (<div />)
     }
-    var latest = this.state.readings[this.state.readings.length - 1].temperature
     return (
       <div className='container'>
         {super.render()}
-        <span className='h6'>Temperature({latest})</span>
-        <ComposedChart width={500} height={250} data={this.state.readings}>
-          <Line type='monotone' dataKey='temperature' stroke='#ce93d8' isAnimationActive={false}  yAxisId="left"/>
-          <YAxis yAxisId="left" orientation="left" />
-          <XAxis/>
+        <span className='h6'>Temperature</span>
+        <ComposedChart width={500} height={250} data={this.state.usage}>
+          <YAxis yAxisId='left' orientation='left' />
+          <YAxis yAxisId='right' orientation='right' />
+          <ReferenceLine yAxisId='right' y={0} />
+          <XAxis />
           <Tooltip />
-          <Bar dataKey='heater' fill='#ffbb33' isAnimationActive={false}  yAxisId="left"/>
-          <Bar dataKey='cooler' fill='#33b5e5' isAnimationActive={false}  yAxisId="left"/>
-          <ReferenceLine y={0} stroke='#0d47a1' />
-          <YAxis label='minutes' yAxisId="right" orientation="right" />
+          <Bar dataKey='heater' fill='#ffbb33' isAnimationActive={false} yAxisId='right' />
+          <Bar dataKey='cooler' fill='#33b5e5' isAnimationActive={false} yAxisId='right' />
+          <Line type='monotone' dataKey='temperature' stroke='#ce93d8' isAnimationActive={false} yAxisId='left' />
         </ComposedChart>
       </div>
     )
