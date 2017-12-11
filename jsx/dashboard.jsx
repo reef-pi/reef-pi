adc8c22d80b86e883a09a2bb72a74daa8a664ce8
Ranjib Dey <ranjib@linux.com>
2017-12-10 20:53:32 -0800

    dont show chart if capability is enabled but sub-system is not

--- jsx/dashboard.jsx
+++ jsx/dashboard.jsx
@@ -11,23 +11,73 @@ import HealthChart from './health_chart.jsx'
 export default class Dashboard extends Common {
   constructor (props) {
     super(props)
+    this.state = {
+      ato: {},
+      tc: {}
+    }
     this.showCharts = this.showCharts.bind(this)
+    this.fetch = this.fetch.bind(this)
+    this.fetchATO = this.fetchATO.bind(this)
+    this.fetchTC = this.fetchTC.bind(this)
+  }
+
+  componentDidMount () {
+    this.fetch()
+  }
+
+  fetchATO () {
+    this.ajaxGet({
+      url: '/api/ato',
+      success: function (data) {
+        this.setState({
+          ato: data,
+          showAlert: false
+        })
+      }.bind(this)
+    })
+  }
+
+  fetchTC () {
+    this.ajaxGet({
+      url: '/api/tc/config',
+      success: function (data) {
+        this.setState({
+          tc: data,
+          showAlert: false
+        })
+      }.bind(this)
+    })
+  }
+
+  fetch () {
+    if (this.props.capabilities.ato) {
+      this.fetchATO()
+    }
+    if (this.props.capabilities.temperature) {
+      this.fetchTC()
+    }
   }
 
   showCharts () {
     var charts = []
-    if (this.props.capabilities.temperature) {
-      charts.push(<div className='col-sm-6' key='chart-0'><TemperatureReadingChart width={500} height={250} /> </div>)
-      charts.push(<div className='col-sm-6' key='chart-1'><TemperatureChart width={500} height={250} /> </div>)
-    }
     if (this.props.capabilities.equipments) {
       charts.push(<div className='col-sm-6' key='chart-2'><EquipmentsChart width={500} height={250} /></div>)
+    }
+    if (this.props.capabilities.temperature) {
+      if (this.state.tc.enable) {
+        charts.push(<div className='col-sm-6' key='chart-0'><TemperatureReadingChart width={500} height={250} /> </div>)
+      }
+      if (this.state.tc.control) {
+        charts.push(<div className='col-sm-6' key='chart-1'><TemperatureChart width={500} height={250} /> </div>)
+      }
     }
     if (this.props.capabilities.lighting) {
       charts.push(<div className='col-sm-6' key='chart-3'><LightsChart width={500} height={250} /></div>)
     }
     if (this.props.capabilities.ato) {
-      charts.push(<div className='col-sm-6' key='chart-4'><ATOChart width={500} height={250} /></div>)
+      if (this.state.ato.enable && this.state.ato.control) {
+        charts.push(<div className='col-sm-6' key='chart-4'><ATOChart width={500} height={250} /></div>)
+      }
     }
     if (this.props.capabilities.health_check) {
       charts.push(<div className='col-sm-6' key='chart-5'><HealthChart width={500} height={250} /></div>)
