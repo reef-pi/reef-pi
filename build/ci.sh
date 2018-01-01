0abcf7b9e203666cce5b09843a1af040932cc332
Ranjib Dey <ranjib@linux.com>
2018-01-01 00:18:50 -0800

    remove debug print statements

--- build/ci.sh
+++ build/ci.sh
@@ -3,7 +3,6 @@
 set -e
 echo "" > coverage.txt
 for d in $(go list ./...); do \
-	echo "Running: go test -race -coverprofile=profile.out -covermode=atomic ${d}"
 	go test -race -coverprofile=profile.out -covermode=atomic $d
 	if [ -f profile.out ]; then
 			cat profile.out >> coverage.txt
