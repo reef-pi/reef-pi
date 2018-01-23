051cebbe2b5f84457b58f6ee79bd2a7806f01f23
Ranjib Dey <ranjib@linux.com>
2018-01-22 20:50:25 -0800

    improve ato sub system test coverage

--- controller/ato/ato_test.go
+++ controller/ato/ato_test.go
@@ -1,11 +1,12 @@
 package ato
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/reef-pi/reef-pi/controller/connectors"
 	"github.com/reef-pi/reef-pi/controller/equipments"
 	"github.com/reef-pi/reef-pi/controller/utils"
 	"testing"
-	"time"
 )
 
 func TestATO(t *testing.T) {
@@ -21,15 +22,59 @@ func TestATO(t *testing.T) {
 		t.Fatal(err)
 	}
 	eqs := equipments.New(conf, outlets, store, telemetry)
+	if err := eqs.Setup(); err != nil {
+		t.Error(err)
+	}
+	if err := outlets.Create(connectors.Outlet{Name: "ato-outlet", Pin: 1}); err != nil {
+		t.Error(err)
+	}
+	if err := eqs.Create(equipments.Equipment{Outlet: "1"}); err != nil {
+		t.Error(err)
+	}
 	c, err := New(true, store, telemetry, eqs)
 
 	if err != nil {
 		t.Error(err)
 	}
-	c.Start()
-	time.Sleep(2 * time.Second)
-	c.Stop()
 	if err := c.Setup(); err != nil {
 		t.Error(err)
 	}
+	c.config.Pump = "1"
+	c.config.Enable = true
+	c.config.Control = true
+	c.Start()
+	tr := utils.NewTestRouter()
+	c.LoadAPI(tr.Router)
+	if err := tr.Do("GET", "/api/ato", new(bytes.Buffer), nil); err != nil {
+		t.Error("Failed to get ato config using api. Error:", err)
+	}
+	_, err = c.Read()
+	if err != nil {
+		t.Error(err)
+	}
+	if err := c.Control(0); err != nil {
+		t.Error(err)
+	}
+	if err := c.Control(1); err != nil {
+		t.Error(err)
+	}
+	c.cachePump()
+	c.updateUsage(10)
+	c.updateUsage(15)
+	c.config.Notify.Enable = true
+	c.NotifyIfNeeded(c.usage.Value.(Usage))
+	body := new(bytes.Buffer)
+	json.NewEncoder(body).Encode(DefaultConfig)
+	if err := tr.Do("POST", "/api/ato", body, nil); err != nil {
+		t.Error("Failed to update ato config using api. Error:", err)
+	}
+
+	if inUse, err := c.IsEquipmentInUse("-1"); err != nil {
+		t.Error(err)
+	} else {
+		if inUse == true {
+			t.Error("Imaginary equipment should not be in-use")
+		}
+	}
+	defer c.Stop()
 }
