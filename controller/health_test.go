ec5524e3131b32ffbd11b80cad8ff3187fc0679f
Ranjib Dey <ranjib@linux.com>
2018-01-24 00:10:52 -0800

    add health check tests. include doser, camrera and lighting subsystem for testing

---
+++ controller/health_test.go
@@ -0,0 +1,19 @@
+package controller
+
+import (
+	"github.com/reef-pi/reef-pi/controller/utils"
+	"testing"
+)
+
+func TestHealthChecker(t *testing.T) {
+	store, err := utils.TestDB()
+	if err != nil {
+		t.Error(err)
+	}
+	telemetry := utils.TestTelemetry()
+	h := NewHealthChecker(1, HealthCheckNotify{}, telemetry, store)
+	h.Notify.Enable = true
+	h.check()
+	h.updateUsage(1, 1)
+
+}
