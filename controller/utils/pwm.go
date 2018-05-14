27eec1419e69ca9b732acb3ecf9a4e3e392108c5
Ranjib Dey <ranjib@linux.com>
2018-05-13 21:01:35 -0700

    mutex protect map writes

--- controller/utils/pwm.go
+++ controller/utils/pwm.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"github.com/reef-pi/drivers"
 	"github.com/reef-pi/rpi/i2c"
+	"sync"
 )
 
 type PWMConfig struct {
@@ -23,6 +24,7 @@ type pca9685Driver struct {
 	values map[int]int
 	driver *drivers.PCA9685
 	config PWMConfig
+	mu     *sync.Mutex
 }
 
 var DefaultPWMConfig = PWMConfig{
@@ -34,6 +36,7 @@ func NewPWM(bus i2c.Bus, config PWMConfig) (PWM, error) {
 		values: make(map[int]int),
 		driver: drivers.NewPCA9685(byte(config.Address), bus),
 		config: config,
+		mu:     &sync.Mutex{},
 	}
 	pwm.driver.Freq = 1500
 	return &pwm, pwm.Start()
@@ -45,6 +48,8 @@ func (p *pca9685Driver) Start() error {
 
 // value should be within 0-99
 func (p *pca9685Driver) Set(pin int, value int) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
 	off := int(float32(value) * 40.96)
 	p.values[pin] = off
 	return p.driver.SetPwm(pin, 0, off)
@@ -64,6 +69,8 @@ func (p *pca9685Driver) On(pin int) error {
 }
 
 func (p *pca9685Driver) Off(pin int) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
 	p.values[pin] = 0
 	return p.driver.SetPwm(pin, 0, 0)
 }
