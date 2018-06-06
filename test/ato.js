7d957c263d6c1629ca8cc6b3e3579559e17e7c89
Ranjib Dey <dey.ranjib@gmail.com>
2018-06-05 23:09:56 -0700

    update smoke test (#390)

--- test/ato.js
+++ test/ato.js
@@ -1,45 +1,42 @@
-const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true })
-
-function TestATO () {
-  nightmare
-    .goto('http://localhost:8080/')
-    .viewport(1100, 650)
+module.exports = {
+  Create: function(n){
+    n.click('li#react-tabs-10')
+    .wait(1000)
+    .click('input#add_new_ato_sensor')
+    .wait(1000)
+    .type('input#new_ato_sensor_name', 'Biocube29')
     .wait(500)
-    .type('input#reef-pi-user', 'reef-pi')
+    .click('button#new_ato-inlet')
     .wait(500)
-    .type('input#reef-pi-pass', 'reef-pi')
+    .click('span#new_ato-2')
     .wait(500)
-    .click('input#btnSaveCreds')
+    .click('input#new_ato_sensor_enable')
     .wait(500)
-
-    .click('li#react-tabs-10')
+    .type('input#new_ato_sensor_period')
+    .type('input#new_ato_sensor_period', 120)
+    .wait(500)
+    .click('input#create_ato')
     .wait(1500)
-    .click('input#ato_enable')
+    return(function(){
+      return('Sign In completed')
+    })
+  },
+  Configure: function(n){
+    n.click('li#react-tabs-10')
+    .wait(500)
+    .click('input#updateATO')
     .wait(500)
     .click('input#ato_control')
     .wait(500)
-    .insert('input#check_interval')
+    .click('button#ato-pump')
     .wait(500)
-    .type('input#check_interval', '60')
+    .click('span#ato-pump-ATOPump')
     .wait(500)
-    .insert('input#sensor_pin')
-    .wait(500)
-    .type('input#sensor_pin', '12')
-    .wait(500)
-    .click('span#ato-pump-Light')
-    .wait(1000)
     .click('input#updateATO')
+    .wait(1500)
+    return(function(){
+      return('ATO configured')
+    })
 
-    .wait(2000)
-    .evaluate(function () { return 'ato' })
-    .end()
-      .then(function (result) {
-        console.log(result)
-      })
-      .catch(function (error) {
-        console.error('Error:', error)
-      })
+  }
 }
-
-TestATO()
---
+++ test/sign_in.js
@@ -0,0 +1,12 @@
+module.exports = {
+  SignIn: function(n){
+    n.type('input#reef-pi-user', 'reef-pi')
+    .wait(500)
+    .type('input#reef-pi-pass', 'reef-pi')
+    .wait(500)
+    .click('button#btnSaveCreds')
+    return(function(){
+      return('Sign In completed')
+    })
+  }
+}
