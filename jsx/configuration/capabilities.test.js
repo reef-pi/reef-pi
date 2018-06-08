ad0e2d033462a7694e3c4f17b30c93c54c095ddf
Ranjib Dey <dey.ranjib@gmail.com>
2018-06-08 03:03:52 -0700

    (chore)add jest based test (#394)
    * add jest based test
    
    * update snapshot during jest run

---
+++ jsx/configuration/capabilities.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Capabilities from './capabilities'
+
+test('render capabilities component', () => {
+  const component = renderer.create(
+    <Capabilities capabilities={[]} update={()=>{}} />
+  )
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+})
