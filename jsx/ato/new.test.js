1cbc3b6a97df054af409df87d45eac8a74b9a947
Ranjib Dey <ranjib@linux.com>
2018-06-17 22:12:07 -0700

    add more test and coverage for javascript code

---
+++ jsx/ato/new.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import 'isomorphic-fetch'
+import Enzyme, {shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import New from './new'
+import configureMockStore from 'redux-mock-store'
+import renderer from 'react-test-renderer'
+import thunk from 'redux-thunk'
+import {mockLocalStorage} from '../utils/test_helper'
+
+Enzyme.configure({ adapter: new Adapter() })
+const mockStore =  configureMockStore([thunk])
+window.localStorage = mockLocalStorage()
+
+describe('New ATO', () => {
+  it('<New />', () => {
+    renderer.create(<New store={mockStore()}/>)
+    const wrapper = shallow(<New store={mockStore()} />)
+    const component = wrapper.dive().instance()
+    component.toggle()
+    component.add()
+    component.setInlet("1")
+  })
+})
