5981e65f708bafcf517d338964693e352b635d6f
Ranjib Dey <ranjib@linux.com>
2018-07-18 13:54:46 -0700

    lint fixes

--- jsx/lighting/lighting.test.js
+++ jsx/lighting/lighting.test.js
@@ -24,7 +24,7 @@ describe('Lighting ui', () => {
         pin: 0,
         profile: {
           type: 'auto',
-          config: {values: [1,2,3,4,5,6,7,8,9,10,11,12]}
+          config: {values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
         }
       }
     }
