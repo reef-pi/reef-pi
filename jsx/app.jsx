316b2fa8daaebd8ff96d737bc6e2986abbe4482b
Ranjib Dey <ranjib@linux.com>
2018-08-05 16:20:36 -0700

    fix equipment update ui

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -2,26 +2,16 @@ import React from 'react'
 import { render } from 'react-dom'
 import MainPanel from 'main_panel'
 import SignIn from 'sign_in'
-import {connect} from 'react-redux'
-import {fetchInfo} from 'redux/actions/info'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
-class app extends React.Component {
-  componentDidMount () {
-    if (SignIn.isSignIned()) {
-      this.props.fetchInfo()
-    }
-  }
-
+export default class App extends React.Component {
   render () {
     if (!SignIn.isSignIned()) {
       return (<SignIn />)
     }
-    var st = {textAlign: 'center'}
     return (
       <div className='container'>
-        <div className='container'><h3 style={st}> {this.props.info.name} </h3></div>
         <div id='reef-pi-alert'/>
         <div className='container'>
           <MainPanel />
@@ -30,14 +20,3 @@ class app extends React.Component {
     )
   }
 }
-
-const mapStateToProps = (state) => {
-  return { info: state.info }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {fetchInfo: () => dispatch(fetchInfo())}
-}
-
-const App = connect(mapStateToProps, mapDispatchToProps)(app)
-export default App
