8e489b2ea9a4f0c37e487d26cd6f905307d022ba
Michael Lane <ranthalion@hotmail.com>
2018-08-19 23:10:35 -0400

    finished profiles, fixed readOnly

--- jsx/lights/light_channel.jsx
+++ jsx/lights/light_channel.jsx
@@ -17,60 +17,64 @@ const Channel = (props) => {
   return (
     <div className="controls border-top">
       <div className="row align-items-start">
-        <div className="col-sm-6 col-md-4">
+        <div className="col-sm-6 col-md-4 col-xl-2">
           <div className="form-group">
             <label>Channel Name</label>
             <input type="text" className="form-control" 
               placeholder="Enter Channel Name"
               name="name"
+              disabled={props.readOnly}
               onChange={handleChange}
               value={props.channel.name} />
           </div>
         </div>
         
-        <div className="form-group col-sm-6 col-md-4 form-inline">
+        <div className="form-group col-sm-6 col-md-4 col-xl-2 form-inline">
           <label className="mb-2">Color</label>
           <ColorPicker name="color"
+            readOnly={props.readOnly}
             color={props.channel.color}
             onChangeHandler={handleChange} />
         </div>
 
-        <div className="col-sm-4">
+        <div className="col-sm-6 col-md-4 col-xl-2">
           <div className="form-group">
             <label>Behavior</label>
             <select className="custom-select"
               name="reverse"
+              disabled={props.readOnly}
               onChange={handleChange} >
               <option value="false">Active High</option>
               <option value="true">Active Low</option>
             </select>
           </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-4">
+        <div className="col-sm-6 col-md-4 col-xl-2">
           <div className="form-group">
             <label>Min</label>
             <input type="text" className="form-control"
               name="min" 
+              disabled={props.readOnly}
               onChange={handleChange}
               value={props.channel.min} />
           </div>  
         </div>
-        <div className="col-sm-4">
+        <div className="col-sm-6 col-md-4 col-xl-2">
           <div className="form-group">
             <label>Max</label>
             <input type="text" className="form-control"
               name="max"
+              disabled={props.readOnly}
               onChange={handleChange}
               value={props.channel.max}  />
           </div>  
         </div>
-        <div className="col-sm-4">
+        <div className="col-sm-6 col-md-4 col-xl-2">
           <div className="form-group">
             <label>Start</label>
             <input type="text" className="form-control"
               name="start_min"
+              disabled={props.readOnly}
               onChange={handleChange} 
               value={props.channel.start_min} />
           </div>  
@@ -82,6 +86,7 @@ const Channel = (props) => {
             <label className="mr-3">Profile</label>
             <ProfileSelector
               name="profile.type"
+              readOnly={props.readOnly}
               onChangeHandler={handleChange}
               value={props.channel.profile.type} />
           </div>
@@ -91,9 +96,19 @@ const Channel = (props) => {
         <div className="col">
           <Profile 
             name='profile.config'
+            readOnly={props.readOnly}
             type={props.channel.profile.type}
             config={props.channel.profile.config} 
             onChangeHandler={handleConfigChange} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="d-sm-none">xs</div>
+          <div className="d-none d-sm-block d-md-none">sm</div>
+          <div className="d-none d-md-block d-lg-none">md</div>
+          <div className="d-none d-lg-block d-xl-none">lg</div>
+          <div className="d-none d-xl-block">xl</div>
         </div>
       </div>
     </div>
