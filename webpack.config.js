58d626b00ca1f5e4504dc7b3fe20d5a93d1e819f
Michael <Ranthalion@hotmail.com>
2018-08-22 16:48:12 -0400

    revert config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 var webpack = require('webpack')
 var path = require('path')
-var LiveReloadPlugin = require('webpack-livereload-plugin')
 
 var BUILD_DIR = path.resolve(__dirname, 'assets/')
 var APP_DIR = path.resolve(__dirname, 'jsx')
 
 var config = {
   entry: APP_DIR + '/entry',
-  mode: 'development',
-  devtool: '#eval-source-map',
   resolve: {
     extensions: ['.js', '.jsx'],
     modules: ['node_modules', path.resolve(__dirname, 'jsx')]
@@ -32,10 +29,7 @@ var config = {
   output: {
     path: BUILD_DIR,
     filename: 'ui.js'
-  },
-  plugins: [
-    new LiveReloadPlugin({delay: 1200})
-  ]
+  }
 
 }
 
