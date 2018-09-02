dfba6bb13f420a84d09a95f5e1275349d84598b6
zekth <g_n_s@hotmail.fr>
2018-09-02 10:47:13 +0200

    Fixing merge

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,13 @@ var path = require("path");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-<<<<<<< HEAD
-// var BUILD_DIR = path.resolve(__dirname, "assets/");
-=======
->>>>>>> upstream/master
 var BUILD_DIR = path.resolve(__dirname, "ui");
 var APP_DIR = path.resolve(__dirname, "jsx");
 
 var config = {
   entry: APP_DIR + "/entry",
   resolve: {
-<<<<<<< HEAD
-    extensions: [".js", ".jsx"],
-=======
     extensions: [".mjs",".js", ".jsx"],
->>>>>>> upstream/master
     modules: [
       "node_modules",
       path.resolve(__dirname, "jsx"),
@@ -31,15 +23,11 @@ var config = {
         include: APP_DIR,
         loader: "babel-loader",
         query: {
-<<<<<<< HEAD
-          presets: ["es2015", "react", "stage-2"]
-=======
           presets: [
             ["es2015", {"modules": false}],
             "react",
             "stage-2"
           ]
->>>>>>> upstream/master
         }
       },
       {
@@ -73,11 +61,6 @@ var config = {
       inject: true
     }),
     new MiniCssExtractPlugin({
-<<<<<<< HEAD
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-=======
->>>>>>> upstream/master
       filename: 'assets/css/[name].css',
       chunkFilename: 'assets/css/[id].css'
     })
