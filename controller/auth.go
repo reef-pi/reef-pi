5808bb53463c6e95344062423b68d097908f6c09
Ranjib Dey <ranjib@linux.com>
2018-09-09 19:56:50 -0700

    use return instead of if/else

--- controller/auth.go
+++ controller/auth.go
@@ -1,10 +1,10 @@
 package controller
 
 import (
-	"github.com/reef-pi/reef-pi/controller/utils"
-	"net/http"
 	"encoding/json"
+	"github.com/reef-pi/reef-pi/controller/utils"
 	"log"
+	"net/http"
 )
 
 type Credentials struct {
@@ -12,45 +12,45 @@ type Credentials struct {
 	Password string `json:"password"`
 }
 
-func (r *ReefPi) Me (w http.ResponseWriter, req *http.Request) {
-  utils.JSONResponse("you?", w, req)
+func (r *ReefPi) Me(w http.ResponseWriter, req *http.Request) {
+	utils.JSONResponse("you?", w, req)
 }
 
 func (r *ReefPi) SignIn(w http.ResponseWriter, req *http.Request) {
-  var reqCredentials Credentials
-  var bucketCredentials Credentials
-  if req.Body == nil {
-    http.Error(w, "No body request", 400)
-    return
-  }
-  if err := json.NewDecoder(req.Body).Decode(&reqCredentials); err != nil {
-    log.Println("DEBUG:", "Error while decoding")
-    http.Error(w, err.Error(), 400)
-    return
-  }
-  session, _ := r.cookiejar.Get(req, "auth")
-  if session.Values["user"] == reqCredentials.User {
-    log.Println("DEBUG:", "Already logged")
-    utils.JSONResponse(nil, w, req)
-	} else {
-    r.store.Get(Bucket, "credentials", &bucketCredentials)
-    if reqCredentials.User == bucketCredentials.User && reqCredentials.Password == bucketCredentials.Password {
-      log.Println("DEBUG:", "Access Granted")
-      session.Values["user"] = reqCredentials.User
-      session.Save(req, w)
-      utils.JSONResponse(nil, w, req)
-    } else {
-      log.Println("DEBUG:", "Access Denied")
-      w.WriteHeader(401)
-      utils.JSONResponse(nil, w, req)
-    }
-  }
+	var reqCredentials Credentials
+	var bucketCredentials Credentials
+	if req.Body == nil {
+		http.Error(w, "No body request", 400)
+		return
+	}
+	if err := json.NewDecoder(req.Body).Decode(&reqCredentials); err != nil {
+		log.Println("DEBUG:", "Error while decoding")
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	session, _ := r.cookiejar.Get(req, "auth")
+	if session.Values["user"] == reqCredentials.User {
+		log.Println("DEBUG:", "Already logged")
+		utils.JSONResponse(nil, w, req)
+		return
+	}
+	r.store.Get(Bucket, "credentials", &bucketCredentials)
+	if reqCredentials.User == bucketCredentials.User && reqCredentials.Password == bucketCredentials.Password {
+		log.Println("DEBUG:", "Access Granted")
+		session.Values["user"] = reqCredentials.User
+		session.Save(req, w)
+		utils.JSONResponse(nil, w, req)
+		return
+	}
+	log.Println("DEBUG:", "Access Denied")
+	w.WriteHeader(401)
+	utils.JSONResponse(nil, w, req)
 }
 
 func (r *ReefPi) SignOut(w http.ResponseWriter, req *http.Request) {
-  session, _ := r.cookiejar.Get(req, "auth")
- 	defer session.Save(req, w)
-  session.Options.MaxAge = -1
+	session, _ := r.cookiejar.Get(req, "auth")
+	defer session.Save(req, w)
+	session.Options.MaxAge = -1
 }
 
 func (r *ReefPi) GetCredentials() (Credentials, error) {
