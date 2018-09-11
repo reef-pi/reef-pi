48a06b8cc6d76161a59c523858f595e86e1fcf4f
zekth <g_n_s@hotmail.fr>
2018-09-11 23:12:53 +0200

    admin panel

--- jsx/configuration/admin.jsx
+++ jsx/configuration/admin.jsx
@@ -1,55 +1,76 @@
-import React from 'react'
-import SignIn from 'sign_in'
-import {confirm} from 'utils/confirm'
-import {reload, reboot, powerOff} from 'redux/actions/admin'
-import {connect} from 'react-redux'
+import React from "react";
+import SignIn from "sign_in";
+import { confirm } from "utils/confirm";
+import { reload, reboot, powerOff } from "redux/actions/admin";
+import { connect } from "react-redux";
 
 class admin extends React.Component {
-  constructor (props) {
-    super(props)
-    this.powerOff = this.powerOff.bind(this)
-    this.reboot = this.reboot.bind(this)
-    this.reload = this.reload.bind(this)
-    this.signout = this.signout.bind(this)
+  constructor(props) {
+    super(props);
+    this.powerOff = this.powerOff.bind(this);
+    this.reboot = this.reboot.bind(this);
+    this.reload = this.reload.bind(this);
+    this.signout = this.signout.bind(this);
   }
 
-  signout () {
-    SignIn.removeCreds()
-    window.location.reload(true)
+  signout() {
+    SignIn.removeCreds();
+    window.location.reload(true);
   }
 
-  reload () {
-    confirm('Are you sure ?').then(this.props.reload)
+  reload() {
+    confirm("Are you sure ?").then(this.props.reload);
   }
 
-  powerOff () {
-    confirm('Are you sure ?').then(this.props.powerOff)
+  powerOff() {
+    confirm("Are you sure ?").then(this.props.powerOff);
   }
 
-  reboot () {
-    confirm('Are you sure ?').then(this.props.reboot)
+  reboot() {
+    confirm("Are you sure ?").then(this.props.reboot);
   }
 
-  render () {
-    const btnClass = 'btn btn-outline-danger col-lg-1 col-xs-3'
+  render() {
+    const btnClass = "btn btn-outline-danger btn-block";
     return (
-      <div className='container'>
-        <button onClick={this.signout} type='button' className={btnClass}>Sign Out</button>
-        <button onClick={this.reload} type='button' className={btnClass}>Reload</button>
-        <button onClick={this.reboot} type='button' className={btnClass}>Reboot</button>
-        <button onClick={this.powerOff} type='button' className={btnClass}>PowerOff</button>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 col-lg-3">
+            <button onClick={this.signout} type="button" className={btnClass}>
+              Sign Out
+            </button>
+          </div>
+          <div className="col-md-12 col-lg-3">
+            <button onClick={this.reload} type="button" className={btnClass}>
+              Reload
+            </button>
+          </div>
+          <div className="col-md-12 col-lg-3">
+            <button onClick={this.reboot} type="button" className={btnClass}>
+              Reboot
+            </button>
+          </div>
+          <div className="col-md-12 col-lg-3">
+            <button onClick={this.powerOff} type="button" className={btnClass}>
+              PowerOff
+            </button>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     reload: () => dispatch(reload()),
     reboot: () => dispatch(reboot()),
     powerOff: () => dispatch(powerOff())
-  }
-}
+  };
+};
 
-const Admin = connect(null, mapDispatchToProps)(admin)
-export default Admin
+const Admin = connect(
+  null,
+  mapDispatchToProps
+)(admin);
+export default Admin;
