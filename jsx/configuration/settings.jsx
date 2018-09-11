5e1b97e9874d04b3aad9856a766d78e149f87579
zekth <g_n_s@hotmail.fr>
2018-09-11 23:35:45 +0200

    settings improvement

--- jsx/configuration/settings.jsx
+++ jsx/configuration/settings.jsx
@@ -1,197 +1,279 @@
-import React from 'react'
-import Capabilities from './capabilities'
-import Display from './display'
-import HealthNotify from './health_notify'
-import {updateSettings, fetchSettings} from 'redux/actions/settings'
-import {connect} from 'react-redux'
-import {isEmptyObject} from 'jquery'
+import React from "react";
+import Capabilities from "./capabilities";
+import Display from "./display";
+import HealthNotify from "./health_notify";
+import { updateSettings, fetchSettings } from "redux/actions/settings";
+import { connect } from "react-redux";
+import { isEmptyObject } from "jquery";
 
 class settings extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       capabilities: props.capabilities,
       settings: {
-        name: '',
-        interface: '',
-        address: '',
+        name: "",
+        interface: "",
+        address: "",
         rpi_pwm_freq: 100,
         pca9685_pwm_freq: 1500
       },
       updated: false
-    }
-    this.updateCheckbox = this.updateCheckbox.bind(this)
-    this.showCapabilities = this.showCapabilities.bind(this)
-    this.updateCapabilities = this.updateCapabilities.bind(this)
-    this.update = this.update.bind(this)
-    this.showDisplay = this.showDisplay.bind(this)
-    this.toRow = this.toRow.bind(this)
-    this.updateHealthNotify = this.updateHealthNotify.bind(this)
-    this.showHealthNotify = this.showHealthNotify.bind(this)
+    };
+    this.updateCheckbox = this.updateCheckbox.bind(this);
+    this.showCapabilities = this.showCapabilities.bind(this);
+    this.updateCapabilities = this.updateCapabilities.bind(this);
+    this.update = this.update.bind(this);
+    this.showDisplay = this.showDisplay.bind(this);
+    this.toRow = this.toRow.bind(this);
+    this.updateHealthNotify = this.updateHealthNotify.bind(this);
+    this.showHealthNotify = this.showHealthNotify.bind(this);
   }
 
-  showHealthNotify () {
+  showHealthNotify() {
     if (this.state.settings.health_check === undefined) {
-      return
+      return;
     }
     if (this.state.settings.capabilities.health_check !== true) {
-      return
+      return;
     }
     return (
-      <HealthNotify update={this.updateHealthNotify} state={this.state.settings.health_check} />
-    )
+      <HealthNotify
+        update={this.updateHealthNotify}
+        state={this.state.settings.health_check}
+      />
+    );
   }
 
-  updateHealthNotify (notify) {
+  updateHealthNotify(notify) {
     if (notify !== undefined) {
-      var settings = this.state.settings
-      settings.health_check = notify
-      this.setState({settings: settings, updated: true})
+      var settings = this.state.settings;
+      settings.health_check = notify;
+      this.setState({ settings: settings, updated: true });
     }
-    return this.state
+    return this.state;
   }
 
-  updateCheckbox (key) {
-    return (function (ev) {
-      var settings = this.state.settings
-      settings[key] = ev.target.checked
+  updateCheckbox(key) {
+    return function(ev) {
+      var settings = this.state.settings;
+      settings[key] = ev.target.checked;
       this.setState({
         settings: settings,
         updated: true
-      })
-    }.bind(this))
+      });
+    }.bind(this);
   }
 
-  showDisplay () {
+  showDisplay() {
     if (!this.state.settings.display) {
-      return
+      return;
     }
-    return (<div className='container'><Display /></div>)
-  }
-
-  showCapabilities () {
     return (
-      <Capabilities capabilities={this.state.capabilities} update={this.updateCapabilities} />
-    )
+      <div className="container">
+        <Display />
+      </div>
+    );
   }
 
-  updateCapabilities (capabilities) {
-    var settings = this.state.settings
-    settings.capabilities = capabilities
+  showCapabilities() {
+    return (
+      <Capabilities
+        capabilities={this.state.capabilities}
+        update={this.updateCapabilities}
+      />
+    );
+  }
+
+  updateCapabilities(capabilities) {
+    var settings = this.state.settings;
+    settings.capabilities = capabilities;
     this.setState({
       settings: settings,
       updated: true
-    })
+    });
   }
 
-  update () {
-    this.props.updateSettings(this.state.settings)
-    this.setState({updated: false})
+  update() {
+    this.props.updateSettings(this.state.settings);
+    this.setState({ updated: false });
   }
 
-  componentDidMount () {
-    this.props.fetchSettings()
+  componentDidMount() {
+    this.props.fetchSettings();
   }
 
-  toRow (label, parse = false) {
-    var fn = function (ev) {
-      var settings = this.state.settings
-      settings[label] = ev.target.value
+  toRow(label, parse = false) {
+    var fn = function(ev) {
+      var settings = this.state.settings;
+      settings[label] = ev.target.value;
       if (parse) {
-        settings[label] = parseInt(settings[label])
+        settings[label] = parseInt(settings[label]);
       }
       this.setState({
         settings: settings,
         updated: true
-      })
-    }.bind(this)
+      });
+    }.bind(this);
     return (
-      <div className='input-group'>
-        <label className='input-group-addon'> {label}</label>
-        <input type='text' onChange={fn} value={this.state.settings[label]} id={'to-row-' + label} />
+      <div className="form-group">
+        <label for={"to-row-" + label}> {label}</label>
+        <input
+          className="form-control"
+          type="text"
+          onChange={fn}
+          value={this.state.settings[label]}
+          id={"to-row-" + label}
+        />
       </div>
-    )
+    );
   }
 
-  static getDerivedStateFromProps (props, state) {
+  static getDerivedStateFromProps(props, state) {
     if (props.settings === undefined) {
-      return null
+      return null;
     }
     if (isEmptyObject(props.settings)) {
-      return null
+      return null;
     }
-    state.settings = props.settings
-    return state
+    state.settings = props.settings;
+    return state;
   }
 
-  render () {
-    var updateButtonClass = 'btn btn-outline-success col-sm-2'
+  render() {
+    var updateButtonClass = "btn btn-outline-success col-sm-2";
     if (this.state.updated) {
-      updateButtonClass = 'btn btn-outline-danger col-sm-2'
+      updateButtonClass = "btn btn-outline-danger col-sm-2";
     }
 
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-sm-6'>
-            {this.toRow('name')}
-            {this.toRow('interface')}
-            {this.toRow('address')}
-            {this.toRow('rpi_pwm_freq', true)}
-            {this.toRow('pca9685_pwm_freq', true)}
-            <div className='input-group'>
-              <label className='input-group-addon'>Notification</label>
-              <input type='checkbox' id='updateNotification' onClick={this.updateCheckbox('notification')} defaultChecked={this.state.settings.notification} className='form-control' />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="row">
+              <div className="col-lg-6 col-sm-12">{this.toRow("name")}</div>
+              <div className="col-lg-6 col-sm-12">
+                {this.toRow("interface")}
+              </div>
             </div>
-            <div className='input-group'>
-              <label className='input-group-addon'>Display</label>
-              <input type='checkbox' id='updateDisplay' onClick={this.updateCheckbox('display')} defaultChecked={this.state.settings.display} className='form-control' />
-              {this.showDisplay()}
+            <div className="row">
+              <div className="col-lg-6 col-sm-12">{this.toRow("address")}</div>
+              <div className="col-lg-6 col-sm-12">
+                {this.toRow("rpi_pwm_freq", true)}
+              </div>
             </div>
-            <div className='input-group'>
-              <label className='input-group-addon'>Use HTTPS</label>
-              <input type='checkbox' id='use_https' onClick={this.updateCheckbox('https')} defaultChecked={this.state.settings.https} className='form-control' />
+            <div className="row">
+              <div className="col-lg-6 col-sm-12">
+                {this.toRow("pca9685_pwm_freq", true)}
+              </div>
+              <div className="col-lg-6 col-sm-12">
+                <div className="form-group">
+                  <label for="updateNotification">Notification</label>
+                  <input
+                    type="checkbox"
+                    id="updateNotification"
+                    onClick={this.updateCheckbox("notification")}
+                    defaultChecked={this.state.settings.notification}
+                    className="form-control"
+                  />
+                </div>
+              </div>
             </div>
-            <div className='input-group'>
-              <label className='input-group-addon'>Enable PCA9685</label>
-              <input type='checkbox' id='enable_pca9685' onClick={this.updateCheckbox('pca9685')} defaultChecked={this.state.settings.pca9685} className='form-control' />
+            <div className="row">
+              <div className="col-lg-6 col-sm-12">
+                <div className="form-group">
+                  <label for="updateDisplay">Display</label>
+                  <input
+                    type="checkbox"
+                    id="updateDisplay"
+                    onClick={this.updateCheckbox("display")}
+                    defaultChecked={this.state.settings.display}
+                    className="form-control"
+                  />
+                  {this.showDisplay()}
+                </div>
+              </div>
+              <div className="col-lg-6 col-sm-12">
+                <div className="form-group">
+                  <label for="use_https">Use HTTPS</label>
+                  <input
+                    type="checkbox"
+                    id="use_https"
+                    onClick={this.updateCheckbox("https")}
+                    defaultChecked={this.state.settings.https}
+                    className="form-control"
+                  />
+                </div>
+              </div>
             </div>
-            <div className='input-group'>
-              <label className='input-group-addon'>Enable profiling</label>
-              <input type='checkbox' id='enable_pprof' onClick={this.updateCheckbox('pprof')} defaultChecked={this.state.settings.pprof} className='form-control' />
+            <div className="row">
+              <div className="col-lg-6 col-sm-12">
+                <div className="form-group">
+                  <label for="enable_pca9685">Enable PCA9685</label>
+                  <input
+                    type="checkbox"
+                    id="enable_pca9685"
+                    onClick={this.updateCheckbox("pca9685")}
+                    defaultChecked={this.state.settings.pca9685}
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <div className="col-lg-6 col-sm-12">
+                <div className="form-group">
+                  <label for="enable_pprof">Enable profiling</label>
+                  <input
+                    type="checkbox"
+                    id="enable_pprof"
+                    onClick={this.updateCheckbox("pprof")}
+                    defaultChecked={this.state.settings.pprof}
+                    className="form-control"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className='row'>
-          <div className='col-sm-4' >
-            <label> <b>Capabilities</b> </label>
+        <div className="row">
+          <div className="col-sm-4">
+            <label>
+              {" "}
+              <b>Capabilities</b>{" "}
+            </label>
             {this.showCapabilities()}
           </div>
         </div>
-        <div className='row'>
-          {this.showHealthNotify()}
-        </div>
-        <div className='row'>
-          <input type='button' className={updateButtonClass} onClick={this.update} id='systemUpdateSettings' value='update' />
+        <div className="row">{this.showHealthNotify()}</div>
+        <div className="row">
+          <input
+            type="button"
+            className={updateButtonClass}
+            onClick={this.update}
+            id="systemUpdateSettings"
+            value="update"
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     capabilities: state.capabilities,
     settings: state.settings
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     fetchSettings: () => dispatch(fetchSettings()),
-    updateSettings: (s) => dispatch(updateSettings(s))
-  }
-}
+    updateSettings: s => dispatch(updateSettings(s))
+  };
+};
 
-const Settings = connect(mapStateToProps, mapDispatchToProps)(settings)
-export default Settings
+const Settings = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(settings);
+export default Settings;
