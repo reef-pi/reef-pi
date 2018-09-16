ade85b8bcde2de7b5a09923b55618048da2fdf58
LE GOFF Vincent <g_n_s@hotmail.fr>
2018-09-16 13:53:00 +0200

    SignIn refactor

--- jsx/sign_in.jsx
+++ jsx/sign_in.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import $ from 'jquery'
 
 export default class SignIn extends React.Component {
   constructor(props) {
@@ -86,55 +87,62 @@ export default class SignIn extends React.Component {
   }
 
   render() {
+    $('html').addClass('auth-page')
+    $('body').addClass('auth-page')
+    $('#main-panel').addClass('auth-page')
     return (
-      <form id="sign-in-form">
-        <div className="container">
-          <div className="form">
-            <h1 className="h3 mb-3 font-weight-normal">reef-pi</h1>
-            {this.state.invalidCredentials ? (
-              <div className="alert alert-danger" role="alert">
-                <strong>Oh snap!</strong> Invalid Credentials
-              </div>
-            ) : (
-              <div />
-            )}
-            <label htmlFor="reef-pi-user" className="sr-only">
-              Username
-            </label>
-            <input
-              onChange={this.handleUserChange}
-              type="text"
-              id="reef-pi-user"
-              className="form-control"
-              name="username"
-              placeholder="Username"
-              required=""
-              autoFocus=""
-            />
-            <label htmlFor="reef-pi-pass" className="sr-only">
-              Password
-            </label>
-            <input
-              onChange={this.handlePasswordChange}
-              type="password"
-              id="reef-pi-pass"
-              className="form-control"
-              name="password"
-              placeholder="Password"
-              required=""
-              autoFocus=""
-            />
-            <button
-              className="btn btn-lg btn-primary btn-block mt-3"
-              onClick={this.login}
-              type="submit"
-              id="btnSaveCreds"
-            >
-              Sign in
-            </button>
+      <div class="container d-flex h-100">
+        <div class="align-self-center w-100">
+          <div class="col-md-12 col-lg-6 mx-auto">
+            <form id="sign-in-form">
+                <div className="form">
+                  <h1 className="h3 mb-3 font-weight-normal reef-pi-title">reef-pi</h1>
+                  {this.state.invalidCredentials ? (
+                    <div className="alert alert-danger" role="alert">
+                      <strong>Oh snap!</strong> Invalid Credentials
+                    </div>
+                  ) : (
+                    <div />
+                  )}
+                  <label htmlFor="reef-pi-user" className="sr-only">
+                    Username
+                  </label>
+                  <input
+                    onChange={this.handleUserChange}
+                    type="text"
+                    id="reef-pi-user"
+                    className="form-control"
+                    name="username"
+                    placeholder="Username"
+                    required=""
+                    autoFocus=""
+                  />
+                  <label htmlFor="reef-pi-pass" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    onChange={this.handlePasswordChange}
+                    type="password"
+                    id="reef-pi-pass"
+                    className="form-control"
+                    name="password"
+                    placeholder="Password"
+                    required=""
+                    autoFocus=""
+                  />
+                  <button
+                    className="btn btn-lg btn-success btn-block mt-3"
+                    onClick={this.login}
+                    type="submit"
+                    id="btnSaveCreds"
+                  >
+                    Sign in
+                  </button>
+                </div>
+            </form>
           </div>
         </div>
-      </form>
+      </div>
     )
   }
 }
