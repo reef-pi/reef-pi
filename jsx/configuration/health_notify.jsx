eac923b72084014c685f3e15c44b5fb7d6856ee5
LE GOFF Vincent <g_n_s@hotmail.fr>
2018-09-16 19:01:12 +0200

    fixing alignement

--- jsx/configuration/health_notify.jsx
+++ jsx/configuration/health_notify.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default class HealthNotify extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       notify: {
@@ -14,64 +14,65 @@ export default class HealthNotify extends React.Component {
     this.updateEnable = this.updateEnable.bind(this)
   }
 
-  updateEnable (ev) {
+  updateEnable(ev) {
     var h = this.state.notify
     h.enable = ev.target.checked
-    this.setState({notify: h})
+    this.setState({ notify: h })
     this.props.update(h)
   }
 
-  update (key) {
-    return (function (ev) {
+  update(key) {
+    return function(ev) {
       var h = this.state.notify
       h[key] = parseInt(ev.target.value)
-      this.setState({notify: h})
+      this.setState({ notify: h })
       this.props.update(h)
-    }.bind(this))
+    }.bind(this)
   }
 
-  render () {
+  render() {
     var ct = [
-      <div className='form-check' key='health_notify_enable'>
-        <label className='form-check-label'>
-          <input
-            className='form-check-input'
-            type='checkbox'
-            id='health_notify_enable'
-            defaultChecked={this.state.notify.enable}
-            onClick={this.updateEnable} />
-          <b>Alert on health check</b>
-        </label>
+      <div className="col-12">
+        <div className="form-check" key="health_notify_enable">
+          <label className="form-check-label">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="health_notify_enable"
+              defaultChecked={this.state.notify.enable}
+              onClick={this.updateEnable}
+            />
+            <b>Alert on health check</b>
+          </label>
+        </div>
       </div>
     ]
     if (this.state.notify.enable) {
       ct.push(
-        <div className='input-group' key='health_notify_max_memory'>
-          <label className='input-group-addon'>Max Memory</label>
+        <div className="form-group col-md-6 col-12" key="health_notify_max_memory">
+          <label htmlFor="health_max_memory">Max Memory</label>
           <input
-            type='text'
-            className='form-control'
-            id='health_max_memory'
+            type="text"
+            className="form-control"
+            id="health_max_memory"
             value={this.state.notify.max_memory}
-            onChange={this.update('max_memory')} />
+            onChange={this.update('max_memory')}
+          />
         </div>
       )
       ct.push(
-        <div className='input-group' key='health_notify_max_cpu'>
-          <label className='input-group-addon'>Max CPU</label>
+        <div className="form-group col-md-6 col-12" key="health_notify_max_cpu">
+          <label htmlFor="health_max_cpu">Max CPU</label>
           <input
-            type='text'
-            className='form-control'
-            id='health_max_cpu'
+            type="text"
+            className="form-control"
+            id="health_max_cpu"
             value={this.state.notify.max_cpu}
-            onChange={this.update('max_cpu')} />
+            onChange={this.update('max_cpu')}
+          />
         </div>
       )
     }
-    return (
-      <div className='col-sm-3'>
-        {ct}
-      </div>
-    )
+    return ct
   }
 }
