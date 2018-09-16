82b22338cbd3df5a5f20d3e9c220639bc3e90ba0
LE GOFF Vincent <g_n_s@hotmail.fr>
2018-09-16 15:55:55 +0200

    Useless container

--- jsx/configuration/main.jsx
+++ jsx/configuration/main.jsx
@@ -16,7 +16,7 @@ const components = {
 }
 
 export default class Configuration extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       body: 'settings'
@@ -24,33 +24,31 @@ export default class Configuration extends React.Component {
     this.setBody = this.setBody.bind(this)
   }
 
-  setBody (k) {
+  setBody(k) {
     return () => {
       this.setState({ body: k })
     }
   }
 
-  render () {
+  render() {
     var panels = []
     let tabs = ['settings', 'connectors', 'telemetry', 'authentication', 'errors', 'admin']
     tabs.forEach((k, _) => {
       var cname = this.state.body === k ? 'nav-item active text-info' : 'nav-item'
       panels.push(
         <li className={cname} key={k}>
-          <a id={'config-' + k} className='nav-link' onClick={this.setBody(k)}>
+          <a id={'config-' + k} className="nav-link" onClick={this.setBody(k)}>
             {k}{' '}
           </a>
         </li>
       )
     })
     var body = components[this.state.body]
-    return (
-      <div className='container'>
-        <div className='row'>
-          <ul className='conf-nav nav nav-tabs'>{panels}</ul>
-        </div>
-        <div className='row'>{body}</div>
-      </div>
-    )
+    return [
+      <div className="row">
+        <ul className="conf-nav nav nav-tabs">{panels}</ul>
+      </div>,
+      <div className="row">{body}</div>
+    ]
   }
 }
