362865b4f7d2981a91d76fcd02b00ed50ca27a87
LE GOFF Vincent <g_n_s@hotmail.fr>
2018-09-19 23:06:23 +0200

    telemtry coverage

--- jsx/telemetry/telmetry.test.js
+++ jsx/telemetry/telmetry.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Enzyme, {shallow } from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import configureMockStore from 'redux-mock-store'
 import Main from './main'
@@ -27,24 +27,36 @@ describe('Telemetry UI', () => {
       throttle: 10,
       adafruitio: aio
     }
-    const m = shallow(<Main store={mockStore({telemetry: telemetry})} />).dive().instance()
+    const m = shallow(<Main store={mockStore({ telemetry: telemetry })} />)
+      .dive()
+      .instance()
     m.updateMailer(mailer)
     const fields = ['server', 'password', 'To', 'From']
-    fields.forEach((k) => {
+    fields.forEach(k => {
       var t = {}
       t[k] = ''
       m.updateMailer(t)
     })
     m.updateAio(aio)
-    m.updateThrottle({target: { value: 20 }})
-    m.enableMailer({target: {checked: true}})
+    m.updateThrottle({ target: { value: 20 } })
+    m.enableMailer({ target: { checked: true } })
     m.save()
+    delete m.state.config.adafruitio
+    m.showAdafruitIO()
+    delete m.state.config.mailer
+    m.notification()
+    delete m.state.config
+    m.showAdafruitIO()
+    m.notification()
+    m.testMessage()
+    m.updateAio({ enable: true, user: '' })
+    m.updateAio({ enable: true, user: 'foo', token: '' })
   })
 
   it('<AdafruitIO />', () => {
     const m = shallow(<AdafruitIO adafruitio={{}} update={() => true} />).instance()
-    m.updateEnable({target: {checked: true}})
-    m.onChange('foo')({target: {value: 1}})
+    m.updateEnable({ target: { checked: true } })
+    m.onChange('foo')({ target: { value: 1 } })
   })
 
   it('<Main />', () => {
