76eaa7af2446c8d712b08e98cd0467c6ed4f5e50
Vincent Le Goff <Vincent.Le_Goff@genesys.com>
2018-09-24 12:53:18 +0200

    app UT

--- jsx/app.test.js
+++ jsx/app.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Enzyme, {shallow} from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import App from './app'
 import configureMockStore from 'redux-mock-store'
 import renderer from 'react-test-renderer'
 import thunk from 'redux-thunk'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import 'isomorphic-fetch'
 import JackSelector from './jack_selector'
 import SelectEquipment from './select_equipment'
@@ -21,24 +21,32 @@ describe('App', () => {
     fetchMock.restore()
   })
   it('<App />', () => {
-    const m = shallow(
-      <App store={mockStore({info: {}, capabilities: []})} />
-    ).instance()
+    SignIn.isSignedIn = jest.fn().mockImplementation(() => {
+      return new Promise(function(res) {
+        return res(true)
+      })
+    })
+    const m = shallow(<App store={mockStore({ info: {}, capabilities: [] })} />).instance()
+    m.state.loaded = true
+    m.render()
+    m.state.logged = true
+    m.getComponent()
+    m.componentDidMount()
   })
 
   it('<JackSelector />', () => {
-    const jacks = [{id: '1', name: 'Foo', pins: [1, 2]}]
-    const m = shallow(
-      <JackSelector store={mockStore({jacks: jacks})} id='1' update={() => {}} />
-    ).dive().instance()
+    const jacks = [{ id: '1', name: 'Foo', pins: [1, 2] }]
+    const m = shallow(<JackSelector store={mockStore({ jacks: jacks })} id="1" update={() => {}} />)
+      .dive()
+      .instance()
     m.setJack(0)
   })
 
   it('<SelectEquipment />', () => {
-    const eqs = [{id: '1', name: 'foo'}]
-    const m = shallow(
-      <SelectEquipment store={mockStore({equipment: eqs})} active='1' update={() => {}} />
-    ).dive().instance()
+    const eqs = [{ id: '1', name: 'foo' }]
+    const m = shallow(<SelectEquipment store={mockStore({ equipment: eqs })} active="1" update={() => {}} />)
+      .dive()
+      .instance()
     m.setEquipment(0)
   })
 })
