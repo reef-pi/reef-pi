e6f9ecb4bda4cf169c008a607044934d59249343
Vincent Le Goff <Vincent.Le_Goff@genesys.com>
2018-09-24 16:34:51 +0200

    Temperature UT

--- jsx/temperature/temperature.test.js
+++ jsx/temperature/temperature.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Enzyme, {shallow } from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import ControlChart from './control_chart'
 import Main from './main'
@@ -17,8 +17,8 @@ const mockStore = configureMockStore([thunk])
 
 describe('Temperature controller ui', () => {
   const state = {
-    tcs: [{id: '1', chart_min: 76, min: 72, max: 78, chart_max: 89}],
-    tc_usage: {'1': {historical: [{cooler: 1}], current: []}}
+    tcs: [{ id: '1', chart_min: 76, min: 72, max: 78, chart_max: 89 }],
+    tc_usage: { '1': { historical: [{ cooler: 1 }], current: [] } }
   }
   it('<Main />', () => {
     shallow(<Main store={mockStore(state)} />).dive()
@@ -28,7 +28,7 @@ describe('Temperature controller ui', () => {
     const fn = () => {}
     const sensors = []
     const equipment = []
-    const tc = {name: 'test', enable: true, sensor: 'sensor1'}
+    const tc = { name: 'test', enable: true, sensor: 'sensor1' }
 
     const m = shallow(<New create={fn} sensors={sensors} equipment={equipment} />).instance()
     m.toggle()
@@ -43,39 +43,50 @@ describe('Temperature controller ui', () => {
       max: 81
     }
     const fn = () => {}
-    const m = shallow(
-      <Sensor data={tc}
-        save={fn}
-        sensors={[]}
-        equipment={[]}
-        remove={fn} />
-    ).instance()
+    const m = shallow(<Sensor data={tc} save={fn} sensors={[]} equipment={[]} remove={fn} />).instance()
     m.save(tc)
     m.expand()
     m.save(tc)
   })
 
   it('<ReadingsChart />', () => {
-    shallow(<ReadingsChart store={mockStore({tcs: [], tc_usage: {'1': {current: []}}})} sensor_id='1' />)
-    shallow(<ReadingsChart store={mockStore(state)} sensor_id='1' />).dive()
+    shallow(<ReadingsChart store={mockStore({ tcs: [], tc_usage: { '1': { current: [] } } })} sensor_id="1" />)
+    shallow(<ReadingsChart store={mockStore(state)} sensor_id="1" />).dive()
   })
 
   it('<ControlChart />', () => {
-    shallow(<ControlChart sensor_id='1' store={mockStore(state)} />).dive()
+    let m = shallow(<ControlChart sensor_id="1" store={mockStore(state)} />)
+      .dive()
+      .instance()
+    m.state.timer = window.setInterval(() => {
+      return true
+    }, 10 * 1000)
+    m.componentWillUnmount()
+    shallow(<ControlChart sensor_id="1" store={mockStore({ tcs: [], tc_usage: [] })} />)
+      .dive()
+      .instance()
+    shallow(
+      <ControlChart
+        sensor_id="1"
+        store={mockStore({ tcs: [{ id: '1', chart_min: 76, min: 72, max: 78, chart_max: 89 }], tc_usage: [] })}
+      />
+    )
+      .dive()
+      .instance()
   })
 
   it('<BooleanSelect /> should bind true', () => {
     let val = ''
     const field = {
       name: 'name',
-      onChange: (e) => {
+      onChange: e => {
         val = e.target.value
       }
     }
     const wrapper = shallow(
       <BooleanSelect field={field}>
-        <option value='true'>Yes</option>
-        <option value='false'>No</option>
+        <option value="true">Yes</option>
+        <option value="false">No</option>
       </BooleanSelect>
     )
     wrapper.find('select').simulate('change', { target: { value: 'true' } })
@@ -86,14 +97,14 @@ describe('Temperature controller ui', () => {
     let val = ''
     const field = {
       name: 'name',
-      onChange: (e) => {
+      onChange: e => {
         val = e.target.value
       }
     }
     const wrapper = shallow(
       <BooleanSelect field={field}>
-        <option value='true'>Yes</option>
-        <option value='false'>No</option>
+        <option value="true">Yes</option>
+        <option value="false">No</option>
       </BooleanSelect>
     )
     wrapper.find('select').simulate('change', { target: { value: 'false' } })
