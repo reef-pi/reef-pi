c9b428589564c5f4e5a741005e4d1411439d1d22
Vincent Le Goff <Vincent.Le_Goff@genesys.com>
2018-10-04 15:29:40 +0200

    lint

--- jsx/utils/alert.js
+++ jsx/utils/alert.js
@@ -1,11 +1,11 @@
 import { configureStore } from 'redux/store'
 import { setUILog } from 'logCenter/log'
 import { addLog } from 'redux/actions/log'
-export function showAlert(msg) {
+export function showAlert (msg) {
   console.warn('showAlert Method is deprecated. Please use notifications/alert')
   configureStore().dispatch(addLog(setUILog('ERROR', msg)))
 }
 
-export function clearAlert() {
+export function clearAlert () {
   console.warning('clearAlert is deprecated.')
 }
