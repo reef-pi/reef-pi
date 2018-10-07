fdeb589fa6e3b5fc920db03283b455b07c28d897
LE GOFF Vincent <g_n_s@hotmail.fr>
2018-10-07 21:31:01 +0200

    Refactor nav

--- jsx/main_panel.jsx
+++ jsx/main_panel.jsx
@@ -34,7 +34,7 @@ const caps = {
 }
 
 class mainPanel extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       tab: 'dashboard'
@@ -42,39 +42,46 @@ class mainPanel extends React.Component {
     this.navs = this.navs.bind(this)
     this.setTab = this.setTab.bind(this)
   }
-  componentDidMount () {
+  componentDidMount() {
     this.props.fetchUIData()
   }
 
-  setTab (k) {
+  setTab(k) {
     return () => {
       this.setState({ tab: k })
     }
   }
 
-  navs (tab) {
+  navs(tab) {
+    let MandatoryTabs = {
+      log: true
+    }
+    let currentCaps = Object.assign(this.props.capabilities, MandatoryTabs)
     var panels = []
     for (let prop in caps) {
-      if (this.props.capabilities[prop] === undefined) {
+      if (currentCaps[prop] === undefined) {
         continue
       }
-      if (!this.props.capabilities[prop]) {
+      if (!currentCaps[prop]) {
         continue
       }
       let cname = prop === tab ? 'nav-link active' : 'nav-link'
       let label = prop
+      if (label.toLowerCase() === 'ato') {
+        label = 'ATO'
+      }
       panels.push(
-        <li className='nav-item' key={'li-tab-' + prop}>
-          <a href='#' id={'tab-' + prop} className={cname} onClick={this.setTab(prop)}>
+        <li className="nav-item" key={'li-tab-' + prop}>
+          <a href="#" id={'tab-' + prop} className={cname} onClick={this.setTab(prop)}>
             {label}
           </a>
         </li>
       )
     }
-    return <ul className='navbar-nav'>{panels}</ul>
+    return <ul className="navbar-nav">{panels}</ul>
   }
 
-  render () {
+  render() {
     var tab = this.state.tab
     if (!this.props.capabilities['dashboard'] && tab === 'dashboard') {
       for (var k in this.props.capabilities) {
@@ -86,37 +93,37 @@ class mainPanel extends React.Component {
     }
     var body = caps[tab]
     return (
-      <div id='content'>
-        <nav className='navbar navbar-dark navbar-reefpi navbar-expand-lg'>
-          <span className='navbar-brand mb-0 h1'>reef-pi</span>
-          <span className='navbar-brand mb-0 h1 navbar-toggler current-tab'>{tab}</span>
+      <div id="content">
+        <nav className="navbar navbar-dark navbar-reefpi navbar-expand-lg">
+          <span className="navbar-brand mb-0 h1">reef-pi</span>
+          <span className="navbar-brand mb-0 h1 navbar-toggler current-tab">{tab}</span>
           <button
-            className='navbar-toggler'
-            type='button'
-            data-toggle='collapse'
-            data-target='#navbarNav'
-            aria-controls='navbarNav'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-            <span className='navbar-toggler-icon' />
+            <span className="navbar-toggler-icon" />
           </button>
           <div
-            className='collapse navbar-collapse navHeaderCollapse'
-            id='navbarNav'
-            data-toggle='collapse'
-            data-target='.navbar-collapse'
+            className="collapse navbar-collapse navHeaderCollapse"
+            id="navbarNav"
+            data-toggle="collapse"
+            data-target=".navbar-collapse"
           >
             {this.navs(tab)}
           </div>
         </nav>
-        <div className='container-fluid'>
+        <div className="container-fluid">
           <NotificationAlert />
-          <div className='row body-panel'>
-            <div className='col-12'>{body}</div>
+          <div className="row body-panel">
+            <div className="col-12">{body}</div>
           </div>
-          <div className='row d-none d-lg-block'>
-            <div className='col-12'>
+          <div className="row d-none d-lg-block">
+            <div className="col-12">
               <Summary fetch={this.props.fetchInfo} info={this.props.info} errors={this.props.errors} />
             </div>
           </div>
