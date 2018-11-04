ffc18f8b32b6acae4d3f334e014e1243e35491ed
Ranjib Dey <dey.ranjib@gmail.com>
2018-11-04 01:54:15 -0800

    (bug)fix diurnal config behavior when time range crosses midnight (#630)
    * fix diurnal config behavior when time range crosses midnight
    
    * fix diurnal logic cross midnigt

--- controller/lighting/profile_test.go
+++ controller/lighting/profile_test.go
@@ -1,47 +1,67 @@
 package lighting
 
 import (
-	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 )
 
-func TestProfile(t *testing.T) {
-	var ch Channel
-	d := DiurnalConfig{
-		Start: "07:30",
-		End:   "18:45",
+func TestDiurnalProfile(t *testing.T) {
+	intensity := Channel{
+		Min:      13,
+		StartMin: 1,
+		Max:      100,
+		Profile: Profile{
+			Type: "diurnal",
+			Config: []byte(
+				`{"start":"10:00",
+		 "end":"19:30"}`),
+		},
 	}
-	d1, err := json.Marshal(&d)
-	if err != nil {
+	spectrum := Channel{
+		Min:      0,
+		StartMin: 1,
+		Max:      100,
+		Profile: Profile{
+			Type: "diurnal",
+			Config: []byte(
+				`{"start":"19:00",
+		 "end":"04:30"}`),
+		},
+	}
+	if err := assertValues("1:30", intensity, spectrum, 0, 100); err != nil {
 		t.Error(err)
 	}
-	t1, err := time.Parse(TimeFormat, "07:00")
-	if err != nil {
+	if err := assertValues("3:30", intensity, spectrum, 0, 50); err != nil {
 		t.Error(err)
 	}
-	ch.Profile.Type = "diurnal"
-	ch.Profile.Config = d1
-	for i := 0; i <= 5*12*4; i++ {
-		ch.GetValue(t1)
-		t1 = t1.Add(time.Duration(5) * time.Minute)
+	if err := assertValues("5:30", intensity, spectrum, 0, 0); err != nil {
+		t.Error(err)
 	}
 
-	ch.Profile.Config = []byte{}
-	if v := ch.GetValueDiurnal(time.Now()); v != 0 {
-		t.Error("Value calculation should return since config is incorrect")
+	if err := assertValues("11:20", intensity, spectrum, 77, 0); err != nil {
+		t.Error(err)
 	}
-	d.Start = ""
-	d2, _ := json.Marshal(&d)
-	ch.Profile.Config = d2
-	if v := ch.GetValueDiurnal(time.Now()); v != 0 {
-		t.Error("Value calculation should return 0 since config is start time is incorrect")
+
+	if err := assertValues("18:00", intensity, spectrum, 85, 0); err != nil {
+		t.Error(err)
 	}
-	d.Start = "07:30"
-	d.End = ""
-	d2, _ = json.Marshal(&d)
-	ch.Profile.Config = d2
-	if v := ch.GetValueDiurnal(time.Now()); v != 0 {
-		t.Error("Value calculation should return 0 since config is end time is incorrect")
+
+	if err := assertValues("21:10", intensity, spectrum, 0, 99); err != nil {
+		t.Error(err)
 	}
+}
+
+func assertValues(t string, ch1, ch2 Channel, v1, v2 int) error {
+	s, err := time.Parse(TimeFormat, t)
+	if err != nil {
+		return err
+	}
+	if v := ch1.GetValue(s); v != v1 {
+		return fmt.Errorf("First channel value is wrong. Execpected %d found: %d", v1, v)
+	}
+	if v := ch2.GetValue(s); v != v2 {
+		return fmt.Errorf("Second channel value is wrong. Execpected %d found: %d", v2, v)
+	}
+	return nil
 }
