ffe70b79de6fb9e15b291b10161d1536a629b005
Ranjib Dey <ranjib@linux.com>
2018-11-18 20:18:25 -0800

    add yup based validation instead of onChange parsing

---
+++ jsx/configuration/settings_schema.test.js
@@ -0,0 +1,35 @@
+import SettingsSchema from './settings_schema'
+
+describe('Settings schema validation', () => {
+  var settings = { }
+
+  beforeEach(() => {
+    settings = {
+      name: 'Hallroom',
+      interface: 'wlan0',
+      address: '0.0.0.0:443',
+      display: false,
+      notification: false,
+      capabilities: {
+        dev_mode: false,
+        dashboard: true
+      },
+      health_check: {
+        enable: false,
+        max_memory: 500,
+        max_cpu: 2
+      },
+      https: true,
+      pca9685: false,
+      pprof: true,
+      rpi_pwm_freq: '20s0',
+      pca9685_pwm_freq: 1500
+    }
+  })
+  it('works', () => {
+    expect.assertions(1)
+    return SettingsSchema.isValid(settings).then(
+      valid => expect(valid).toBe(true)
+    )
+  })
+})
