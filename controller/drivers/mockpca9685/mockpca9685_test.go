dbaf7e7d5486941fa0af640a23596c89a92e27ac
Yann Ramin <atrus@stackworks.net>
2018-12-06 07:51:52 -0800

    Remove two passes of testify/assert

--- controller/drivers/mockpca9685/mockpca9685_test.go
+++ controller/drivers/mockpca9685/mockpca9685_test.go
@@ -7,34 +7,53 @@ import (
 
 	"github.com/reef-pi/reef-pi/controller/settings"
 	"github.com/reef-pi/reef-pi/controller/types/driver"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestNewMockDriver(t *testing.T) {
 	drvr, err := NewMockDriver(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
-
+	if err != nil {
+		t.Errorf("mock driver error %v", err)
+	}
 	pwmDriver, ok := drvr.(driver.PWM)
-	assert.True(t, ok)
+	if !ok {
+		t.Error("driver is now a PWM driver")
+	}
 
 	outputDriver, ok := drvr.(driver.Output)
-	assert.True(t, ok)
+	if !ok {
+		t.Error("driver is not an output driver")
+	}
 
-	assert.Len(t, pwmDriver.PWMChannels(), 8)
-	assert.Len(t, outputDriver.OutputPins(), 8)
+	if l := len(pwmDriver.PWMChannels()); l != 8 {
+		t.Errorf("unexpected number of mock channels: %d", l)
+	}
+
+	if l := len(outputDriver.OutputPins()); l != 8 {
+		t.Errorf("unexpected number of mock pins: %d", l)
+	}
 
 	_, err = pwmDriver.GetPWMChannel("does not exist")
-	assert.Error(t, err)
+	if err == nil {
+		t.Error("expected error on non-existent channel")
+	}
 
 	_, err = pwmDriver.GetPWMChannel("1")
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("error getting valid channel %v", err)
+	}
 
 	_, err = outputDriver.GetOutputPin("does not exist either")
-	assert.Error(t, err)
+	if err == nil {
+		t.Error("expected error on non-existent pin")
+	}
 
 	_, err = outputDriver.GetOutputPin("1")
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("error getting valid pin %v", err)
+	}
 
 	err = drvr.Close()
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("error closing driver %v", err)
+	}
 }
--- controller/drivers/mockrpi/mock_test.go
+++ controller/drivers/mockrpi/mock_test.go
@@ -7,96 +7,134 @@ import (
 
 	"github.com/reef-pi/reef-pi/controller/settings"
 	"github.com/reef-pi/reef-pi/controller/types/driver"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestNewMockDriver(t *testing.T) {
 	drvr, err := NewMockDriver(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("unexpected error making mock driver %v", err)
+	}
 
 	_, ok := drvr.(driver.Input)
-	assert.True(t, ok)
+	if !ok {
+		t.Error("mock driver is not an input driver")
+	}
 
 	_, ok = drvr.(driver.Output)
-	assert.True(t, ok)
+	if !ok {
+		t.Error("mock driver is not an output driver")
+	}
 
 	_, ok = drvr.(driver.PWM)
-	assert.True(t, ok)
+	if !ok {
+		t.Error("mock driver is not a PWM driver")
+	}
 }
 
 func TestMockDriver_GetInputPin(t *testing.T) {
 	drvr, err := NewMockDriver(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("unexpected error making mock driver %v", err)
+	}
 
 	in, ok := drvr.(driver.Input)
-	assert.True(t, ok)
+	if !ok {
+		t.Error("mock driver is not an input driver")
+	}
 
 	pin, err := in.GetInputPin("GP21")
-	assert.NoError(t, err)
-	assert.NotNil(t, pin)
+	if err != nil {
+		t.Errorf("error getting mock pin GP21: %v", err)
+	}
+	if pin == nil {
+		t.Error("pin was nil")
+	}
 
 	v, err := pin.Read()
-	assert.NoError(t, err)
-	assert.False(t, v)
+	if err != nil {
+		t.Errorf("pin read error %v", err)
+	}
+	if v != false {
+		t.Error("pin read returned true")
+	}
 }
 
 func TestMockDriver_InputPins(t *testing.T) {
 	drvr, err := NewMockDriver(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("error making mock driver %v", err)
+	}
 
-	in, ok := drvr.(driver.Input)
-	assert.True(t, ok)
-
+	in := drvr.(driver.Input)
 	pins := in.InputPins()
-	assert.Len(t, pins, 6)
+	if l := len(pins); l != 6 {
+		t.Errorf("unexpected number of pins, got %d", l)
+	}
 }
 
 func TestMockDriver_GetOutputPin(t *testing.T) {
 	drvr, err := NewMockDriver(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("error making mock driver %v", err)
+	}
 
-	in, ok := drvr.(driver.Output)
-	assert.True(t, ok)
+	in := drvr.(driver.Output)
 
 	pin, err := in.GetOutputPin("GP21")
-	assert.NoError(t, err)
-	assert.NotNil(t, pin)
+	if err != nil {
+		t.Errorf("can't get output pin GP21 %v", err)
+	}
 
-	assert.NoError(t, pin.Write(true))
-	assert.True(t, pin.LastState())
+	if err := pin.Write(true); err != nil {
+		t.Errorf("can't write to pin %v", err)
+	}
+	if pin.LastState() != true {
+		t.Errorf("pin didn't toggle to on")
+	}
 }
 
 func TestMockDriver_OutputPins(t *testing.T) {
 	drvr, err := NewMockDriver(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("error making mock driver %v", err)
+	}
 
-	in, ok := drvr.(driver.Output)
-	assert.True(t, ok)
-
+	in := drvr.(driver.Output)
 	pins := in.OutputPins()
-	assert.Len(t, pins, 6)
+
+	if l := len(pins); l != 6 {
+		t.Errorf("expected 6 pins, got %d", l)
+	}
 }
 
 func TestMockDriver_PWMChannels(t *testing.T) {
 	drvr, err := NewMockDriver(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("error making mock driver %v", err)
+	}
 
-	pwm, ok := drvr.(driver.PWM)
-	assert.True(t, ok)
-
-	assert.Len(t, pwm.PWMChannels(), 2)
+	pwm := drvr.(driver.PWM)
+	if l := len(pwm.PWMChannels()); l != 2 {
+		t.Errorf("expected 2 PWM channels, got %d", l)
+	}
 }
 
 func TestMockDriver_GetPWMChannel(t *testing.T) {
 	drvr, err := NewMockDriver(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("error making mock driver %v", err)
+	}
 
-	pwm, ok := drvr.(driver.PWM)
-	assert.True(t, ok)
-
+	pwm := drvr.(driver.PWM)
 	ch, err := pwm.GetPWMChannel("0")
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("can't get PWM channel '0'")
+	}
 
-	assert.NoError(t, ch.Set(100))
-	assert.Error(t, ch.Set(101))
+	if err := ch.Set(100); err != nil {
+		t.Error("unexpected error setting PWM to 100")
+	}
+	if err := ch.Set(101); err == nil {
+		t.Error("didn't get error when setting PWM to 101")
+	}
 }
