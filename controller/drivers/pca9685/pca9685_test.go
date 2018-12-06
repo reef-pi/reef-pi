0841b90a9c5ae8968b6717f01719393eb3f7609a
Yann Ramin <atrus@stackworks.net>
2018-12-06 09:16:12 -0800

    Remove testify from pca9685

--- controller/drivers/pca9685/pca9685_test.go
+++ controller/drivers/pca9685/pca9685_test.go
@@ -6,56 +6,80 @@ import (
 	"github.com/reef-pi/reef-pi/controller/settings"
 	driverif "github.com/reef-pi/reef-pi/controller/types/driver"
 	"github.com/reef-pi/rpi/i2c"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestNewPCA9685(t *testing.T) {
 	driver, err := NewPCA9685(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("unexpected error making driver %v", err)
+	}
 
 	meta := driver.Metadata()
-	assert.Equal(t, "pca9685", meta.Name)
-	assert.True(t, meta.Capabilities.PWM)
+	if meta.Name != "pca9685" {
+		t.Errorf("name %s did not match pca9685", meta.Name)
+	}
+	if !meta.Capabilities.PWM {
+		t.Error("driver didn't indicate it supports PWM")
+	}
 
 	pwmDriver, ok := driver.(driverif.PWM)
-	assert.True(t, ok)
-	assert.NotNil(t, pwmDriver)
+	if !ok {
+		t.Error("driver is not a PWM interface")
+	}
+	if pwmDriver == nil {
+		t.Error("driver is nil")
+	}
 
 	channels := pwmDriver.PWMChannels()
-	assert.Len(t, channels, 16)
+	if l := len(channels); l != 16 {
+		t.Errorf("expected 16 channels, got %d", l)
+	}
 
 	channel15, err := pwmDriver.GetPWMChannel("15")
-	assert.NoError(t, err)
-	assert.NotNil(t, channel15)
+	if err != nil {
+		t.Errorf("error fetching channel 15 %v", err)
+	}
+	if channel15 == nil {
+		t.Error("nil pwm driver")
+	}
 }
 
 func TestPca9685Channel_Set(t *testing.T) {
 	driver, err := NewPCA9685(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("unexpected error making driver %v", err)
+	}
 
 	pwmDriver := driver.(driverif.PWM)
-	assert.NotNil(t, pwmDriver)
-
 	channel15, err := pwmDriver.GetPWMChannel("15")
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("error fetching channel 15 %v", err)
+	}
 
 	err = channel15.Set(50)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Error("can't set channel to 50%")
+	}
 
 	err = channel15.Set(150)
-	assert.Errorf(t, err, "invalid range didn't produce error")
+	if err == nil {
+		t.Error("channel 15 allowed setting 150%")
+	}
 
 	err = channel15.Set(-1)
-	assert.Errorf(t, err, "invalid range didn't produce error")
+	if err == nil {
+		t.Error("channel 15 allowed setting -1%")
+	}
 }
 
 func TestPca9685Driver_Close(t *testing.T) {
 	driver, err := NewPCA9685(settings.DefaultSettings, i2c.MockBus())
-	assert.NoError(t, err)
-
-	pwmDriver := driver.(driverif.PWM)
-	assert.NotNil(t, pwmDriver)
+	if err != nil {
+		t.Errorf("unexpected error making driver %v", err)
+	}
 
 	err = driver.Close()
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("unexpected error closing driver %v", err)
+	}
 }
