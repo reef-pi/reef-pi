844a745dbfdeee9cc5ca2e3ed5c7c632c8c5f15b
Ranjib Dey <ranjib@linux.com>
2019-01-12 00:15:02 -0800

    change driver summary in connectors

--- jsx/connectors/outlet.jsx
+++ jsx/connectors/outlet.jsx
@@ -127,7 +127,10 @@ export default class Outlet extends React.Component {
       <div className='row'>
         <div className='col-4'>{this.state.name}</div>
         <div className='col-1'>
-          <label className='small'>{this.state.pin}</label>
+          <label className='small'>
+            {this.state.driver_name}
+            ({this.state.pin})
+          </label>
         </div>
         <div className='col'>
           <label className='small'>{this.props.equipment === '' ? '' : 'in-use'}</label>
@@ -137,9 +140,6 @@ export default class Outlet extends React.Component {
         </div>
         <div className='col'>
           <label className='small'>{this.state.reverse ? 'reverse' : ''}</label>
-        </div>
-        <div className='col'>
-          <label className='small'>{this.state.driver_name}</label>
         </div>
       </div>
     )
