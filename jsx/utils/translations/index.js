6014fbe27dbde33243f75f160e86618f0922b0ca
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-02-17 12:56:35 +0100

    Translation addition

--- jsx/utils/translations/index.js
+++ jsx/utils/translations/index.js
@@ -1,7 +1,17 @@
 import translationsEN from './en.json'
 import translationsFR from './fr.json'
+import translationsDE from './de.json'
+import translationsIT from './it.json'
+import translationsFA from './fa.json'
+import translationsHI from './hi.json'
+import translationsES from './es.json'
 const ts = {
   en: translationsEN,
-  fr: translationsFR
+  fr: translationsFR,
+  de: translationsDE,
+  it: translationsIT,
+  fa: translationsFA,
+  hi: translationsHI,
+  es: translationsES
 }
 export default ts
