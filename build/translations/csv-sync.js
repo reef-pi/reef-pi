4cd8a72baeae8ce30498e82bab0f502a8c1f1f11
Vincent LE GOFF <g_n_s@hotmail.fr>
2019-02-18 11:50:11 +0100

    Handling the deprecation of translation keys

--- build/translations/csv-sync.js
+++ build/translations/csv-sync.js
@@ -4,22 +4,34 @@ const files = fs.readdirSync(PATH_CSV)
 const masterFile = 'en.csv'
 const MasterTranslations = fs.readFileSync(`${PATH_CSV}${masterFile}`, 'utf8').match(/[^\r\n]+/g)
 MasterTranslations.shift()
-function parseSlaveTranslation(masterLine, slaveArray) {
+function updateMissingTranslation(masterLine, slaveArray) {
   let masterKey = masterLine.split(',')[0]
   let sK = slaveArray.find(s => masterKey === s.split(',')[0])
   if (!sK) {
     slaveArray.push(`${masterKey},##`)
   }
 }
+function deleteDeprecatedTranslations(slaveLine, masterArray) {
+  let slaveKey = slaveLine.split(',')[0]
+  let found = masterArray.find(s => slaveKey === s.split(',')[0])
+  if (!found) {
+    console.log(`Deprecated key: ${slaveKey}`)
+    slaveLine = ''
+  }
+  return slaveLine
+}
 files.forEach(f => {
   if (f !== masterFile) {
     let slave = fs.readFileSync(`${PATH_CSV}${f}`, 'utf8').match(/[^\r\n]+/g)
     slave.shift()
     MasterTranslations.forEach(t => {
-      parseSlaveTranslation(t, slave)
+      updateMissingTranslation(t, slave)
     })
+    for (let i = 0; i < slave.length; i++) {
+      slave[i]= deleteDeprecatedTranslations(slave[i], MasterTranslations)
+    }
     slave.unshift('Key,Translation')
     let csvOutput = slave.join('\r\n')
-    fs.writeFileSync(`${PATH_CSV}${f}`,csvOutput)
+    fs.writeFileSync(`${PATH_CSV}${f}`, csvOutput)
   }
 })
