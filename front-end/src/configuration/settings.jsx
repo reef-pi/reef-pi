9e14c4af1209a4fdaa87c8c17d3c91780bb9cb6e
Michael Lane <ranthalion@hotmail.com>
2019-10-08 20:33:51 -0400

    Move protocol and reset if contradictory

--- front-end/src/configuration/settings.jsx
+++ front-end/src/configuration/settings.jsx
@@ -32,6 +32,9 @@ class settings extends React.Component {
     this.updateHealthNotify = this.updateHealthNotify.bind(this)
     this.showHealthNotify = this.showHealthNotify.bind(this)
     this.handleSetLang = this.handleSetLang.bind(this)
+    this.handleSetAddress = this.handleSetAddress.bind(this)
+    this.handleSetProtocolHttp = this.handleSetProtocolHttp.bind(this)
+    this.handleSetProtocolHttps = this.handleSetProtocolHttps.bind(this)
   }
 
   handleSetLang (ev) {
@@ -68,6 +71,45 @@ class settings extends React.Component {
         updated: true
       })
     }.bind(this)
+  }
+
+  handleSetAddress (ev) {
+    const settings = this.state.settings
+    settings.address = ev.target.value
+    this.setState({
+      settings: settings,
+      updated: true
+    })
+  }
+
+  handleSetProtocolHttp () {
+    this.handleSetProtocol(false)
+  }
+
+  handleSetProtocolHttps () {
+    this.handleSetProtocol(true)
+  }
+
+  handleSetProtocol (value) {
+    const settings = this.state.settings
+    const port = settings.address.split(':')[1]
+
+    // Technically redundant, but improves readability
+    // Remove the port if https and port 80
+    // Also remove the port if http and port 443
+    if (port === '80' && value === true) {
+      const address = settings.address.split(':')[0]
+      settings.address = address // Remove port
+    } else if (port === '443' && value === false) {
+      const address = settings.address.split(':')[0]
+      settings.address = address // Remove port
+    }
+
+    settings.https = value
+    this.setState({
+      settings: settings,
+      updated: true
+    })
   }
 
   showDisplay () {
@@ -160,7 +202,29 @@ class settings extends React.Component {
               <div className='col-lg-6 col-sm-12'>{this.toRow('interface')}</div>
             </div>
             <div className='row'>
-              <div className='col-lg-6 col-sm-12'>{this.toRow('address')}</div>
+              <div className='col-lg-6 col-sm-12'>
+                <div className='form-group'>
+                  <label htmlFor='to-row-address'> {i18n.t('configuration:settings:address')}</label>
+                  <div className='input-group'>
+                    <div className='input-group-prepend'>
+                      <button className='btn btn-outline-secondary dropdown-toggle' type='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                        {this.state.settings.https === true ? 'https' : 'http'}://
+                      </button>
+                      <div className='dropdown-menu'>
+                        <a className='dropdown-item' onClick={this.handleSetProtocolHttp}>http://</a>
+                        <a className='dropdown-item' onClick={this.handleSetProtocolHttps}>https://</a>
+                      </div>
+                    </div>
+                    <input
+                      className='form-control'
+                      type='text'
+                      onChange={this.handleSetAddress}
+                      value={this.state.settings.address}
+                      id='to-row-address'
+                    />
+                  </div>
+                </div>
+              </div>
               <div className='col-lg-6 col-sm-12'>{this.toRow('rpi_pwm_freq')}</div>
             </div>
             <div className='row'>
@@ -180,18 +244,6 @@ class settings extends React.Component {
                   </select>
                 </div>
               </div>
-              <div className='col-lg-6 col-sm-12'>
-                <div className='form-group'>
-                  <label htmlFor='updateNotification'>{i18n.t('configuration:settings:notification')}</label>
-                  <input
-                    type='checkbox'
-                    id='updateNotification'
-                    onClick={this.updateCheckbox('notification')}
-                    defaultChecked={this.state.settings.notification}
-                    className='form-control'
-                  />
-                </div>
-              </div>
             </div>
             <div className='row'>
               <div className='col-6'>
@@ -207,14 +259,14 @@ class settings extends React.Component {
                   {this.showDisplay()}
                 </div>
               </div>
-              <div className='col-6'>
+              <div className='col-lg-6 col-sm-12'>
                 <div className='form-group'>
-                  <label htmlFor='use_https'>{i18n.t('configuration:settings:use_https')}</label>
+                  <label htmlFor='updateNotification'>{i18n.t('configuration:settings:notification')}</label>
                   <input
                     type='checkbox'
-                    id='use_https'
-                    onClick={this.updateCheckbox('https')}
-                    defaultChecked={this.state.settings.https}
+                    id='updateNotification'
+                    onClick={this.updateCheckbox('notification')}
+                    defaultChecked={this.state.settings.notification}
                     className='form-control'
                   />
                 </div>
@@ -233,8 +285,6 @@ class settings extends React.Component {
                   />
                 </div>
               </div>
-            </div>
-            <div className='row'>
               <div className='col-6'>
                 <div className='form-group'>
                   <label htmlFor='enable_prometheus'>{i18n.t('configuration:settings:enable_prometheus')}</label>
