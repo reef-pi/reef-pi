decc94638ab5e864d579858674a62ce6828c0018
Ranjib Dey <ranjib@linux.com>
2019-10-10 20:02:36 -0700

    add arbitrary interval profile (#849)

---
+++ controller/pwm_profile/interval_test.go
@@ -0,0 +1,36 @@
+package pwm_profile
+
+import (
+	"testing"
+	"time"
+)
+
+func TestInterval(t *testing.T) {
+	tFormat := "15:04"
+	conf := `
+{
+	"start":"10:00",
+	"end": "19:30",
+	"interval": 60,
+	"values": [0,10,30,40,50,60,80,40,20,10]
+}
+`
+	i, err := Interval([]byte(conf), 13, 100)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t1, err := time.Parse(tFormat, "10:30")
+	if err != nil {
+		t.Error(err)
+	}
+	if int(i.Get(t1)) != 0 {
+		t.Error("Expected 0, got:", i.Get(t1))
+	}
+	t2, err := time.Parse(tFormat, "16:30")
+	if err != nil {
+		t.Error(err)
+	}
+	if int(i.Get(t2)) != 60 {
+		t.Error("Expected 60, got:", i.Get(t2))
+	}
+}
