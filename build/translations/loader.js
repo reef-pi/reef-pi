d5b13891714b6c7894df097aaad162a8d26cf6ae
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-11-28 22:18:42 +0100

    Add csv loader

--- build/translations/loader.js
+++ build/translations/loader.js
@@ -1,7 +1,11 @@
 const { genTsObj } = require('./csv-json')
+const parse = require('csv-parse/lib/sync')
 
 exports.default = function(source) {
-  const lines = source.match(/[^\r\n]+/g)
-  const output = genTsObj(lines)
+  const records = parse(source, {
+    columns: true,
+    skip_empty_lines: true
+  })
+  const output = genTsObj(records)
   return `export default ${JSON.stringify(output)}`
 }
