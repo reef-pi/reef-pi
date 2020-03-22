981792ae6b7f487262b53908217ae1133ec016c7
Ranjib Dey <ranjib@linux.com>
2020-03-22 00:38:42 -0700

    update teletime test

--- controller/telemetry/stats_test.go
+++ controller/telemetry/stats_test.go
@@ -1,55 +1,39 @@
 package telemetry
 
 import (
-	"encoding/json"
-	"testing"
-
 	"github.com/reef-pi/reef-pi/controller/storage"
+	"testing"
 )
 
-type testMetric struct {
-	Value int
-}
-
-func (m1 *testMetric) Before(m Metric) bool {
-	return true
-}
-
-func (m1 *testMetric) Rollup(m Metric) (Metric, bool) {
-	return m1, true
-}
-
-func TestStatsManager(t *testing.T) {
+func TestEmitMetric(t *testing.T) {
 	store, err := storage.TestDB()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	defer store.Close()
 
-	store.CreateBucket("test-subsystem")
-	mgr := TestTelemetry(store).NewStatsManager("test-subsystem")
-	metric := &testMetric{}
-	mgr.Update("foo", metric)
-	mgr.Update("foo", metric)
-	if err := mgr.Save("foo"); err != nil {
-		t.Error(err)
-	}
-	fn := func(_ json.RawMessage) interface{} { return metric }
-	if err := mgr.Load("foo", fn); err != nil {
-		t.Error(err)
-	}
-	if err := mgr.Delete("foo"); err != nil {
-		t.Error(err)
-	}
-}
-
-func TestInitialize(t *testing.T) {
-	store, err := storage.TestDB()
+	tele := TestTelemetry(store)
+	tele.EmitMetric("test", "foo", 1.23)
+	tele.config.Throttle = 2
+	sent, err := tele.Alert("test-alert", "")
 	if err != nil {
 		t.Error(err)
 	}
-	defer store.Close()
-
-	logger := func(_, _ string) error { return nil }
-	Initialize("telemetry", store, logger, true)
+	if !sent {
+		t.Error("Test alert not sent")
+	}
+	sent, err = tele.Alert("test-alert", "")
+	if err != nil {
+		t.Error(err)
+	}
+	if !sent {
+		t.Error("Test alert not sent")
+	}
+	sent, err = tele.Alert("test-alert", "")
+	if err != nil {
+		t.Error(err)
+	}
+	if sent {
+		t.Error("Test alert not being throttled")
+	}
 }
