18ab98bf735a7d54c8a40ce14a92fcf30d2a7ccb
Ranjib Dey <ranjib@linux.com>
2020-03-30 02:37:54 -0700

    coerce string value for booleans

--- front-end/src/drivers/edit_driver.jsx
+++ front-end/src/drivers/edit_driver.jsx
@@ -52,7 +52,7 @@ const EditDriver = ({
                 name={'config.' + item.name.toLowerCase()}
                 disabled={readOnly}
                 type={item.type == 4 ? 'checkbox' : 'text'}
-                placeholder={item.default}
+                placeholder={item.default.toString()}
                 className={classNames('form-control', {
                   'is-invalid': ShowError('config.' + item.name.toLowerCase(), touched, errors)
                 })}
