7d04677fe80e91c367a1656508725d2ae654fcdc
Ranjib Dey <ranjib@linux.com>
2020-05-21 02:21:13 -0700

    add support for diurnal and fixed profile

--- front-end/src/lighting/chart.jsx
+++ front-end/src/lighting/chart.jsx
@@ -1,61 +1,32 @@
 import React from 'react'
-import { ResponsiveContainer, Tooltip, XAxis, YAxis, LineChart, Line } from 'recharts'
 import { connect } from 'react-redux'
+import IntervalChart from './charts/interval'
+import FixedChart from './charts/fixed'
+import DiurnalChart from './charts/diurnal'
 
 class chart extends React.Component {
-  constructor (props) {
-    super(props)
-    this.channel2line = this.channel2line.bind(this)
-  }
-
-  channel2line (ch, data) {
-    const stroke = ch.color === '' ? '#000' : ch.color
-    switch (ch.profile.type) {
-      case 'interval':
-        if (ch.profile.config) {
-          ch.profile.config.values.forEach((value, i) => {
-            if (data[i] === undefined) {
-              data[i] = { time: i * 2 + 'h' }
-            }
-            data[i][ch.name] = value
-          })
-          return <Line dataKey={ch.name} stroke={stroke} key={ch.pin} isAnimationActive={false} />
-        }
-        break
-      case 'fixed':
-        data[0] = {
-          time: 'Always'
-        }
-        data[0][ch.name] = ch.profile.config.value
-        return <Line dataKey={ch.name} stroke={stroke} key={ch.pin} isAnimationActive={false} />
-      default:
-        console.log('supported chart ', ch.profile.type)
-    }
-  }
-
   render () {
     if (this.props.config === undefined) {
-      return <div />
+      return <span />
     }
-    const data = []
-    const lines = Object.keys(this.props.config.channels).map((ch) =>
-      this.channel2line(this.props.config.channels[ch], data)
-    )
-    return (
-      <div className='container'>
-        <span className='h6'>Light - {this.props.config.name}</span>
-        <ResponsiveContainer height={this.props.height} width='100%'>
-          <LineChart data={data}>
-            <XAxis dataKey='time' />
-            <YAxis />
-            <Tooltip />
-            {lines}
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-    )
+    const keys = Object.keys(this.props.config.channels)
+    if (keys.length > 1) {
+      return (<span> multi channel light charts are not supported</span>)
+    }
+    const ch = this.props.config.channels[keys[0]]
+    switch (ch.profile.type) {
+      case 'interval':
+        return <IntervalChart channel={ch} width={this.props.width} height={this.props.height} />
+      case 'fixed':
+        return <FixedChart channel={ch} width={this.props.width} height={this.props.height} />
+      case 'diurnal':
+        return <DiurnalChart channel={ch} width={this.props.width} height={this.props.height} />
+      default:
+        return (<span>Unsupported light chart for profile: <b>{ch.profile.type}</b></span>)
+    }
   }
 }
+
 const mapStateToProps = (state, ownProps) => {
   return {
     config: state.lights.find(l => {
