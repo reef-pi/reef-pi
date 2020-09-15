f46b5a27c2fe47eb660d9aac1eb9a3d6ab9193df
Ranjib Dey <ranjib@linux.com>
2020-09-15 00:53:48 -0700

    link api doc in summary information

--- front-end/src/summary.jsx
+++ front-end/src/summary.jsx
@@ -25,6 +25,7 @@ export default class Summary extends React.Component {
           <li className='list-inline-item'>{i18n.t('running')} {this.props.info.version}, on {this.props.info.model}</li>
           <li className='list-inline-item'>{i18n.t('since')} {this.props.info.uptime} | </li>
           <li className='list-inline-item'>IP {this.props.info.ip} | </li>
+          <li className='list-inline-item'><a href='/assets/api.html'>API</a> | </li>
           <li className='list-inline-item text-danger'>{i18n.t('errors')}({this.props.errors.length})</li>
         </ul>
       </nav>
