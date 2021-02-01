7e9a8fa3d19259fdf883f578746d573f9a6420cd
Ranjib Dey <ranjib@linux.com>
2021-02-01 01:43:49 -0800

    why you dont like meathods

--- controller/daemon/api.go
+++ controller/daemon/api.go
@@ -40,7 +40,7 @@ func (r *ReefPi) API() error {
 		router.Use(func(next http.Handler) http.Handler {
 			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Access-Control-Allow-Origin", "*")
-				w.Header().Set("Access-Control-Allow-Meathods", "*")
+				w.Header().Set("Access-Control-Allow-Methods", "*")
 				w.Header().Set("Access-Control-Allow-Headers", "*")
 				next.ServeHTTP(w, r)
 			})
