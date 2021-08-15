afba3ccdd5726d93875ce95dc07d10c34f22e944
Ranjib Dey <ranjib@linux.com>
2021-08-14 17:23:42 -0700

    fix modal , disable lint for that specific line

--- front-end/src/ph/calibration_wizard.jsx
+++ front-end/src/ph/calibration_wizard.jsx
@@ -110,7 +110,7 @@ export default class CalibrationWizard extends React.Component {
         <div className='modal-footer'>
           <div className='text-center'>
             {cancelButton}
-            <button role='confirm' type='button' className='btn btn-primary' ref={this.props.confirm} onClick={this.handleConfirm}>
+            <button role='confirm' type='button' className='btn btn-primary' ref='confirm' onClick={this.handleConfirm}> // eslint-disable-line no-eval
               {i18next.t('done')}
             </button>
           </div>
