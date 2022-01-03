7c59f0660f6d602d6287038e019e5b0f78cdded0
Schwabix-1311 <git@kuhn-clan.de>
2022-01-03 17:48:04 +0100

    Driver validation
    Changed validation messages to a generic set, translatable.
    Added few missing UI element translations.

--- front-end/src/drivers/driver_schema.jsx
+++ front-end/src/drivers/driver_schema.jsx
@@ -1,14 +1,15 @@
 import * as Yup from 'yup'
+import i18n from 'utils/i18n'
 
 const DriverSchema = Yup.object().shape({
   name: Yup.string()
-    .required('Name is required'),
+    .required(i18n.t('validation:name_required')),
   type: Yup.string()
-    .required('Type is required'),
+    .required(i18n.t('validation:selection_required')),
   config: Yup.lazy(value => {
     const shape = {}
     Object.keys(value).forEach(prop => {
-      shape[prop] = Yup.string().required(prop + ' is required')
+      shape[prop] = Yup.string().required(i18n.t('validation:entry_required'))
     })
     return Yup.object().shape(shape)
   })
