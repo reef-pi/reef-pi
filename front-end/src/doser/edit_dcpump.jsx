079f2fbaa8305268e1a49696df272e4243e0a698
Ranjib Dey <ranjib@linux.com>
2022-01-15 22:21:37 -0800

    i18n wrapper migration

--- front-end/src/doser/edit_dcpump.jsx
+++ front-end/src/doser/edit_dcpump.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ErrorFor, ShowError } from '../utils/validation_helper'
 import { Field } from 'formik'
-import i18next from 'i18next'
+import i18n from 'utils/i18n'
 import classNames from 'classnames'
 import BooleanSelect from '../ui_components/boolean_select'
 import Percent from '../ui_components/percent'
@@ -51,7 +51,7 @@ const EditDcPump = ({
     <div className='row'>
       <div className='col-12 col-sm-6 col-md-3'>
         <div className='form-group'>
-          <label htmlFor='jack'>{i18next.t('jack')}</label>
+          <label htmlFor='jack'>{i18n.t('jack')}</label>
           <Field
             name='jack'
             component='select'
@@ -61,7 +61,7 @@ const EditDcPump = ({
               'is-invalid': ShowError('jack', touched, errors)
             })}
           >
-            <option value='' className='d-none'>-- {i18next.t('select')} --</option>
+            <option value='' className='d-none'>-- {i18n.t('select')} --</option>
             {jackOptions()}
           </Field>
           <ErrorFor errors={errors} touched={touched} name='jack' />
@@ -70,7 +70,7 @@ const EditDcPump = ({
 
       <div className='col-12 col-sm-6 col-md-3'>
         <div className='form-group'>
-          <label htmlFor='pin'>{i18next.t('pin')}</label>
+          <label htmlFor='pin'>{i18n.t('pin')}</label>
           <Field
             name='pin'
             component='select'
@@ -79,7 +79,7 @@ const EditDcPump = ({
               'is-invalid': ShowError('pin', touched, errors)
             })}
           >
-            <option value='' className='d-none'>-- {i18next.t('select')} --</option>
+            <option value='' className='d-none'>-- {i18n.t('select')} --</option>
             {pinOptions()}
           </Field>
           <ErrorFor errors={errors} touched={touched} name='pin' />
@@ -88,7 +88,7 @@ const EditDcPump = ({
 
       <div className='col-12 col-sm-6 col-md-3'>
         <div className='form-group'>
-          <label htmlFor='enable'>{i18next.t('status')}</label>
+          <label htmlFor='enable'>{i18n.t('status')}</label>
           <Field
             name='enable'
             component={BooleanSelect}
@@ -97,15 +97,15 @@ const EditDcPump = ({
               'is-invalid': ShowError('enable', touched, errors)
             })}
           >
-            <option value='true'>{i18next.t('enabled')}</option>
-            <option value='false'>{i18next.t('disabled')}</option>
+            <option value='true'>{i18n.t('enabled')}</option>
+            <option value='false'>{i18n.t('disabled')}</option>
           </Field>
           <ErrorFor errors={errors} touched={touched} name='enable' />
         </div>
       </div>
       <div className='col-12 col-sm-6 col-md-3'>
         <div className='form-group'>
-          <label htmlFor='duration'>{i18next.t('doser:duration')}</label>
+          <label htmlFor='duration'>{i18n.t('doser:duration')}</label>
           <div className='input-group'>
             <Field
               name='duration'
@@ -117,7 +117,7 @@ const EditDcPump = ({
             />
             <div className='input-group-append'>
               <span className='input-group-text d-none d-lg-flex'>
-                {i18next.t('second_s')}
+                {i18n.t('second_s')}
               </span>
               <span className='input-group-text d-flex d-lg-none'>sec</span>
             </div>
@@ -128,7 +128,7 @@ const EditDcPump = ({
 
       <div className='col col-sm-6 col-md-3'>
         <div className='form-group'>
-          <label htmlFor='speed'>{i18next.t('doser:speed')}</label>
+          <label htmlFor='speed'>{i18n.t('doser:speed')}</label>
           <div className='input-group'>
             <Percent
               type='number'
--- front-end/src/doser/edit_stepper.jsx
+++ front-end/src/doser/edit_stepper.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ErrorFor, ShowError } from '../utils/validation_helper'
 import { Field } from 'formik'
-import i18next from 'i18next'
+import i18n from 'utils/i18n'
 import classNames from 'classnames'
 import BooleanSelect from '../ui_components/boolean_select'
 
@@ -31,7 +31,7 @@ const EditStepper = ({
     <div className='row'>
       <div className='col'>
         <div className='form-group'>
-          <label htmlFor='step_pin'>{i18next.t('doser:step_pin')}</label>
+          <label htmlFor='step_pin'>{i18n.t('doser:step_pin')}</label>
           <Field
             name='stepper.step_pin'
             disabled={readOnly}
@@ -40,7 +40,7 @@ const EditStepper = ({
               'is-invalid': ShowError('stepper.step_pin', touched, errors)
             })}
           >
-            <option value='' className='d-none'>-- {i18next.t('select')} --</option>
+            <option value='' className='d-none'>-- {i18n.t('select')} --</option>
             {outletOptions()}
           </Field>
           <ErrorFor errors={errors} touched={touched} name='stepper.step_pin' />
@@ -49,7 +49,7 @@ const EditStepper = ({
 
       <div className='col'>
         <div className='form-group'>
-          <label htmlFor='direction_pin'>{i18next.t('doser:direction_pin')}</label>
+          <label htmlFor='direction_pin'>{i18n.t('doser:direction_pin')}</label>
           <Field
             name='stepper.direction_pin'
             disabled={readOnly}
@@ -58,7 +58,7 @@ const EditStepper = ({
               'is-invalid': ShowError('stepper.direction_pin', touched, errors)
             })}
           >
-            <option value='' className='d-none'>-- {i18next.t('select')} --</option>
+            <option value='' className='d-none'>-- {i18n.t('select')} --</option>
             {outletOptions()}
           </Field>
           <ErrorFor errors={errors} touched={touched} name='stepper.direction_pin' />
@@ -67,7 +67,7 @@ const EditStepper = ({
 
       <div className='col'>
         <div className='form-group'>
-          <label htmlFor='ms_pin_a'>{i18next.t('doser:ms_pin_a')}</label>
+          <label htmlFor='ms_pin_a'>{i18n.t('doser:ms_pin_a')}</label>
           <Field
             name='stepper.ms_pin_a'
             disabled={readOnly}
@@ -76,7 +76,7 @@ const EditStepper = ({
               'is-invalid': ShowError('stepper.ms_pin_a', touched, errors)
             })}
           >
-            <option value='' className='d-none'>-- {i18next.t('select')} --</option>
+            <option value='' className='d-none'>-- {i18n.t('select')} --</option>
             {outletOptions()}
           </Field>
           <ErrorFor errors={errors} touched={touched} name='stepper.ms_pin_a' />
@@ -85,7 +85,7 @@ const EditStepper = ({
 
       <div className='col'>
         <div className='form-group'>
-          <label htmlFor='ms_pin_b'>{i18next.t('doser:ms_pin_b')}</label>
+          <label htmlFor='ms_pin_b'>{i18n.t('doser:ms_pin_b')}</label>
           <Field
             name='stepper.ms_pin_b'
             disabled={readOnly}
@@ -94,7 +94,7 @@ const EditStepper = ({
               'is-invalid': ShowError('stepper.ms_pin_b', touched, errors)
             })}
           >
-            <option value='' className='d-none'>-- {i18next.t('select')} --</option>
+            <option value='' className='d-none'>-- {i18n.t('select')} --</option>
             {outletOptions()}
           </Field>
           <ErrorFor errors={errors} touched={touched} name='stepper.ms_pin_b' />
@@ -103,7 +103,7 @@ const EditStepper = ({
 
       <div className='col'>
         <div className='form-group'>
-          <label htmlFor='ms_pin_c'>{i18next.t('doser:ms_pin_c')}</label>
+          <label htmlFor='ms_pin_c'>{i18n.t('doser:ms_pin_c')}</label>
           <Field
             name='stepper.ms_pin_c'
             disabled={readOnly}
@@ -112,7 +112,7 @@ const EditStepper = ({
               'is-invalid': ShowError('stepper.ms_pin_c', touched, errors)
             })}
           >
-            <option value='' className='d-none'>-- {i18next.t('select')} --</option>
+            <option value='' className='d-none'>-- {i18n.t('select')} --</option>
             {outletOptions()}
           </Field>
           <ErrorFor errors={errors} touched={touched} name='stepper.ms_pin_c' />
@@ -121,7 +121,7 @@ const EditStepper = ({
 
       <div className='col'>
         <div className='form-group'>
-          <label htmlFor='spr'>{i18next.t('doser:spr')}</label>
+          <label htmlFor='spr'>{i18n.t('doser:spr')}</label>
           <Field
             name='stepper.spr'
             disabled={readOnly}
@@ -136,7 +136,7 @@ const EditStepper = ({
 
       <div className='col'>
         <div className='form-group'>
-          <label htmlFor='vpr'>{i18next.t('stepper:vpr')}</label>
+          <label htmlFor='vpr'>{i18n.t('stepper:vpr')}</label>
           <Field
             name='stepper.vpr'
             disabled={readOnly}
@@ -151,7 +151,7 @@ const EditStepper = ({
 
       <div className='col'>
         <div className='form-group'>
-          <label htmlFor='delay'>{i18next.t('stepper:delay')}</label>
+          <label htmlFor='delay'>{i18n.t('stepper:delay')}</label>
           <Field
             name='stepper.delay'
             disabled={readOnly}
@@ -166,7 +166,7 @@ const EditStepper = ({
 
       <div className='col'>
         <div className='form-group'>
-          <label htmlFor='direction'>{i18next.t('doser:direction')}</label>
+          <label htmlFor='direction'>{i18n.t('doser:direction')}</label>
           <Field
             name='stepper.direction'
             disabled={readOnly}
@@ -175,8 +175,8 @@ const EditStepper = ({
               'is-invalid': ShowError('stepper.direction', touched, errors)
             })}
           >
-            <option value='true'>{i18next.t('forward')}</option>
-            <option value='false'>{i18next.t('reverse')}</option>
+            <option value='true'>{i18n.t('forward')}</option>
+            <option value='false'>{i18n.t('reverse')}</option>
           </Field>
           <ErrorFor errors={errors} touched={touched} name='stepper.direction' />
         </div>
@@ -184,7 +184,7 @@ const EditStepper = ({
 
       <div className='col'>
         <div className='form-group'>
-          <label htmlFor='microstepping'>{i18next.t('doser:microstepping')}</label>
+          <label htmlFor='microstepping'>{i18n.t('doser:microstepping')}</label>
           <Field
             name='stepper.microstepping'
             disabled={readOnly}
@@ -193,12 +193,12 @@ const EditStepper = ({
               'is-invalid': ShowError('stepper.microstepping', touched, errors)
             })}
           >
-            <option value='Full'>{i18next.t('stepper:microstep:full')}</option>
-            <option value='Half'>{i18next.t('stepper:microstep:1/2')}</option>
-            <option value='1/4'>{i18next.t('stepper:microstep:1/4')}</option>
-            <option value='1/8'>{i18next.t('stepper:microstep:1/8')}</option>
-            <option value='1/16'>{i18next.t('stepper:microstep:1/16')}</option>
-            <option value='1/32'>{i18next.t('stepper:microstep:1/32')}</option>
+            <option value='Full'>{i18n.t('stepper:microstep:full')}</option>
+            <option value='Half'>{i18n.t('stepper:microstep:1/2')}</option>
+            <option value='1/4'>{i18n.t('stepper:microstep:1/4')}</option>
+            <option value='1/8'>{i18n.t('stepper:microstep:1/8')}</option>
+            <option value='1/16'>{i18n.t('stepper:microstep:1/16')}</option>
+            <option value='1/32'>{i18n.t('stepper:microstep:1/32')}</option>
           </Field>
           <ErrorFor errors={errors} touched={touched} name='stepper.microstepping' />
         </div>
