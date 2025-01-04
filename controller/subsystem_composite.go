0f91d8a7f389d0a96677f00bf9c7066c95bd2042
Hector Espert <hectorespertpardo@gmail.com>
2025-01-04 18:56:26 +0100

    Logging subsystem

--- controller/subsystem_composite.go
+++ controller/subsystem_composite.go
@@ -1,69 +1,71 @@
 package controller
 
 import (
-	"fmt"
-	"log"
-	"sync"
+    "fmt"
+    "log"
+    "sync"
 
-	"github.com/gorilla/mux"
+    "github.com/gorilla/mux"
 )
 
 type SubsystemComposite struct {
-	mu         *sync.RWMutex
-	components map[string]Subsystem
+    mu         *sync.RWMutex
+    components map[string]Subsystem
 }
 
 func NewSubsystemComposite() *SubsystemComposite {
-	return &SubsystemComposite{
-		mu:         new(sync.RWMutex),
-		components: make(map[string]Subsystem),
-	}
+    return &SubsystemComposite{
+        mu:         new(sync.RWMutex),
+        components: make(map[string]Subsystem),
+    }
 }
 
 func (s *SubsystemComposite) Unload(name string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	delete(s.components, name)
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    delete(s.components, name)
 }
 
 func (s *SubsystemComposite) UnloadAll() {
-	for sName, sController := range s.components {
-		sController.Stop()
-		s.Unload(sName)
-		log.Println("Successfully unloaded module:", sName)
-	}
+    for sName, sController := range s.components {
+        log.Println("Unloading module:", sName)
+        sController.Stop()
+        s.Unload(sName)
+        log.Println("Successfully unloaded module:", sName)
+    }
 }
 
 func (s *SubsystemComposite) Load(name string, sub Subsystem) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.components[name] = sub
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    s.components[name] = sub
 }
 
 func (s *SubsystemComposite) Sub(name string) (Subsystem, error) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	sub, ok := s.components[name]
-	if !ok {
-		return nil, fmt.Errorf("Subsystem not present: %s", name)
-	}
-	return sub, nil
+    s.mu.RLock()
+    defer s.mu.RUnlock()
+    sub, ok := s.components[name]
+    if !ok {
+        return nil, fmt.Errorf("subsystem not present: %s", name)
+    }
+    return sub, nil
 }
 
 func (s *SubsystemComposite) LoadAPI(router *mux.Router) {
-	for _, sController := range s.components {
-		sController.LoadAPI(router)
-	}
+    for _, sController := range s.components {
+        sController.LoadAPI(router)
+    }
 }
 
 func (s *SubsystemComposite) Setup() error {
-	for sName, sController := range s.components {
-		if err := sController.Setup(); err != nil {
-			log.Println("ERROR: Failed to setup subsystem:", sName)
-			return fmt.Errorf("%s:%w", sName, err)
-		}
-		sController.Start()
-		log.Println("Successfully started subsystem:", sName)
-	}
-	return nil
+    for sName, sController := range s.components {
+        log.Println("Starting subsystem:", sName)
+        if err := sController.Setup(); err != nil {
+            log.Println("ERROR: Failed to setup subsystem:", sName)
+            return fmt.Errorf("%s:%w", sName, err)
+        }
+        sController.Start()
+        log.Println("Successfully started subsystem:", sName)
+    }
+    return nil
 }
--- controller/subsystem_composite_test.go
+++ controller/subsystem_composite_test.go
@@ -3,30 +3,30 @@ package controller
 import "testing"
 
 func TestSubsystemComposite(t *testing.T) {
-    s := NewSubsystemComposite()
+	s := NewSubsystemComposite()
 
-    if _, err := s.Sub("noop"); err == nil {
-        t.Error("Expected error for unknown subsystem")
-    }
+	if _, err := s.Sub("noop"); err == nil {
+		t.Error("Expected error for unknown subsystem")
+	}
 
-    s.Load("noop", NoopSubsystem())
-    if _, err := s.Sub("noop"); err != nil {
-        t.Error("Expected no error for known subsystem")
-    }
-    s.Unload("noop")
-    if _, err := s.Sub("noop"); err == nil {
-        t.Error("Expected error for unknown subsystem")
-    }
+	s.Load("noop", NoopSubsystem())
+	if _, err := s.Sub("noop"); err != nil {
+		t.Error("Expected no error for known subsystem")
+	}
+	s.Unload("noop")
+	if _, err := s.Sub("noop"); err == nil {
+		t.Error("Expected error for unknown subsystem")
+	}
 
-    s.Load("noop", NoopSubsystem())
+	s.Load("noop", NoopSubsystem())
 
-    if err := s.Setup(); err != nil {
-        t.Error(err)
-    }
+	if err := s.Setup(); err != nil {
+		t.Error(err)
+	}
 
-    s.UnloadAll()
+	s.UnloadAll()
 
-    if _, err := s.Sub("noop"); err == nil {
-        t.Error("Expected error for unknown subsystem")
-    }
+	if _, err := s.Sub("noop"); err == nil {
+		t.Error("Expected error for unknown subsystem")
+	}
 }
