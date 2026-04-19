ab99f4abc2fe6e2dc8e85245a22257b2f6897071
Ranjib Dey <ranjib@linux.com>
2026-04-19 00:14:04 -0700

    [codex] Fix React 19 form and calibration tests

--- front-end/src/health_chart.test.js
+++ front-end/src/health_chart.test.js
@@ -1,24 +1,45 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import HealthChart from './health_chart'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
+import { RawHealthChart } from './health_chart'
 import 'isomorphic-fetch'
 
-const mockStore = configureMockStore([thunk])
-
 describe('Health', () => {
-  it('<HealthChart />', () => {
-    const state = {
-      health_stats: {},
-      timer: window.setInterval(() => true, 10)
-    }
-    const m = shallow(<HealthChart store={mockStore(state)} />).dive().instance()
+  const fetchHealth = jest.fn()
+
+  beforeEach(() => {
+    jest.spyOn(window, 'setInterval').mockReturnValue(123)
+    jest.spyOn(window, 'clearInterval').mockImplementation(() => {})
   })
-  it('<HealthChart />', () => {
-    const state = {
-      trend: 'current'
-    }
-    const m = shallow(<HealthChart store={mockStore(state)} />).dive().instance()
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+    jest.clearAllMocks()
+  })
+
+  it('fetches health data on mount and renders empty state when data is missing', () => {
+    const chart = new RawHealthChart({
+      fetchHealth,
+      health_stats: {},
+      height: 100
+    })
+
+    expect(window.setInterval).toHaveBeenCalledWith(fetchHealth, 60 * 1000)
+
+    chart.componentDidMount()
+
+    expect(fetchHealth).toHaveBeenCalledTimes(1)
+    expect(chart.render().type).toBe('div')
+  })
+
+  it('clears the polling timer on unmount', () => {
+    const chart = new RawHealthChart({
+      fetchHealth,
+      health_stats: { current: [] },
+      trend: 'current',
+      height: 100
+    })
+
+    chart.componentWillUnmount()
+
+    expect(window.clearInterval).toHaveBeenCalledWith(123)
   })
 })
