9d220dfa1f28196622f50e52602986845b165aa2
Ranjib Dey <ranjib@linux.com>
2026-05-06 21:55:56 -0700

    Make journal unit requirement explicit

--- front-end/src/journal/schema.js
+++ front-end/src/journal/schema.js
@@ -1,13 +1,16 @@
 import * as Yup from 'yup'
 import i18n from 'utils/i18n'
 
+const entryRequiredMessage = i18n.t('validation:entry_required')
+const unitRequiredMessage = entryRequiredMessage
+
 const JournalSchema = Yup.object().shape({
   name: Yup.string()
     .required(i18n.t('validation:name_required')),
   description: Yup.string()
-    .required(i18n.t('validation:entry_required')),
+    .required(entryRequiredMessage),
   unit: Yup.string()
-    .required(i18n.t('validation:entry_required')) // TODO: really required?
+    .required(unitRequiredMessage)
 })
 
 export default JournalSchema
